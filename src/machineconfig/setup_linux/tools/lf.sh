e5dff256bb0cab795496bccb1297242ecc6fe9c6
Alex Al-Saffar <programmer@usa.com>
2022-09-18 07:40:04 +0930

    made self sufficient lf installation

--- src/machineconfig/setup_linux/tools/lf.sh
+++ src/machineconfig/setup_linux/tools/lf.sh
@@ -1,5 +1,12 @@
 
-source $(dirname $0)/get_latest_release.sh
+# source $(dirname $0)/get_latest_release.sh
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
 r=$(get_latest_release "gokcehan/lf")
 
 cd ~
