dedee03a5cf3169968624cc7a33b5c3112a22cfa
Alex Al-Saffar <programmer@usa.com>
2022-09-25 05:59:12 +0930

    minor bugs

--- src/machineconfig/scripts/python/tmate_conn.py
+++ src/machineconfig/scripts/python/tmate_conn.py
@@ -1,21 +1,24 @@
 
 import argparse
-from crocodile.toolbox import SSH
+import configparser
+from pathlib import Path
+import random
+import string
+import os
 
 
 def main():
-    parser = argparse.ArgumentParser(description='FTP client')
+    creds = configparser.ConfigParser()
+    creds.read(Path.home().joinpath('dotfiles/creds/tmate/creds.ini'))
 
-    parser.add_argument("machine", help=f"machine ssh address", default="")
-    parser.add_argument("file", help="file/folder path.", default="")
-    # FLAGS
-    parser.add_argument("--recursive", "-r", help="Send recursively.", action="store_true")  # default is False
-    parser.add_argument("--zipFirst", "-z", help="Zip before sending.", action="store_true")  # default is False
+    parser = argparse.ArgumentParser(description='Tmate launcher')
+    parser.add_argument("sess_name", help=f"session name", default=random.choices((string.digits + string.ascii_letters).split(''), k=20))
 
     args = parser.parse_args()
-    ssh = SSH(rf'{args.machine}')
-    ssh.copy_from_here(args.file, zip_first=args.zipFirst, r=args.recursive)
-    ssh.print_summary()
+    sess_name = creds['sesssions_names'][args.sess_name]
+    api_key = creds['keys']['api_key']
+    res = f"tmate -a ~/.ssh/authorized_keys -k {api_key} -n {sess_name} -F"
+    os.system(res)
 
 
 if __name__ == '__main__':
