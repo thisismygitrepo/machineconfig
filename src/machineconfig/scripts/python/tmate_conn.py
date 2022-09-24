2f4b911c178baeb95f67da9bad0184c820ae88d7
Alex Al-Saffar <programmer@usa.com>
2022-09-25 06:20:43 +0930

    bug fixes

--- src/machineconfig/scripts/python/tmate_conn.py
+++ src/machineconfig/scripts/python/tmate_conn.py
@@ -12,10 +12,10 @@ def main():
     creds.read(Path.home().joinpath('dotfiles/creds/tmate/creds.ini'))
 
     parser = argparse.ArgumentParser(description='Tmate launcher')
-    parser.add_argument("sess_name", help=f"session name", default=random.choices((string.digits + string.ascii_letters).split(''), k=20))
+    parser.add_argument("sess_name", help=f"session name", default=random.choices(list(string.digits + string.ascii_letters), k=20))
 
     args = parser.parse_args()
-    sess_name = creds['sesssions_names'][args.sess_name]
+    sess_name = creds['sessions_names'][args.sess_name]
     api_key = creds['keys']['api_key']
     res = f"tmate -a ~/.ssh/authorized_keys -k {api_key} -n {sess_name} -F"
     os.system(res)
