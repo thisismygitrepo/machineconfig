2cc39081b0b38816a0062918be382f50bf60867a
Alex Al-Saffar <programmer@usa.com>
2022-09-27 17:01:30 +0930

    fixed a bug

--- src/machineconfig/scripts/windows/change_shell_theme.ps1
+++ src/machineconfig/scripts/windows/change_shell_theme.ps1
@@ -8,9 +8,8 @@ else {
     $name = $args[0]
 }
 
-cd ~/code/machineconfig/src/machineconfig
 ~/venvs/ve/Scripts/Activate.ps1
-python -m fire ./setup_windows_terminal/fancy_prompt_themes.py choose $args[0]
+python -m fire machineconfig.setup_windows_terminal.ohmyposh choose $args[0]
 .$profile  # reload the profile
 
 cd $tmp
