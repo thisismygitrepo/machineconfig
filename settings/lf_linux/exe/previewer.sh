93c1944e7d904c00d93b34f72d6cfbdad8876094
Alex Al-Saffar <programmer@usa.com>
2022-09-27 20:32:47 +0930

    fixed bugs

--- settings/lf_linux/exe/previewer.sh
+++ settings/lf_linux/exe/previewer.sh
@@ -1,6 +1,6 @@
 
 clear
-batcat --color=always --theme=base16 $args[0]
+batcat --color=always --theme=base16 $1
 
 #Write-Host -NoNewLine 'Press any key to continue...';
 #$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
