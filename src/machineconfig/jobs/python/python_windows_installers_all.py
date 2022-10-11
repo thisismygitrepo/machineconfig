43b5da14d21f3bd18d0aeed91995d5d8d3750fb2
Alex Al-Saffar <programmer@usa.com>
2022-10-12 00:39:59 +1030

    fixed minor bug

--- src/machineconfig/jobs/python/python_windows_installers_all.py
+++ src/machineconfig/jobs/python/python_windows_installers_all.py
@@ -3,5 +3,5 @@ import crocodile.toolbox as tb
 import machineconfig.jobs.python_windows_installers as inst
 import machineconfig.jobs.python_generic_installers as gens
 
-tb.P(inst.__file__).parent.search().apply(tb.Read.py)
-tb.P(gens.__file__).parent.search().apply(tb.Read.py)
+tb.P(inst.__file__).parent.search("*.py").apply(tb.Read.py)
+tb.P(gens.__file__).parent.search("*.py").apply(tb.Read.py)
