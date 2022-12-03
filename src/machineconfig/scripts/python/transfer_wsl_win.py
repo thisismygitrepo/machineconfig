88aac19351f6c7f801059ca2a89e8e765099f37b
Alex Al-Saffar <programmer@usa.com>
2022-12-03 15:28:42 +1030

    avoided dangerous direct file moving between windows and wsl

--- src/machineconfig/scripts/python/transfer_wsl_win.py
+++ src/machineconfig/scripts/python/transfer_wsl_win.py
@@ -12,18 +12,23 @@ Otherwise, a flag must be raised to indicate the direction.""")
     # positional argument
     parser.add_argument("path", help="path of file/folder to transfer over.")
     # FLAGS
-    parser.add_argument("--move", "-m", help="Move file.", action="store_true")  # default is False
+    # this is dangerous and no gaurantee on no corruption.
+    parser.add_argument("--same_file_system", "-s", help="Move file.", action="store_true")  # default is False
     # optional argument
-    parser.add_argument("--destination", "-d", help="New path.", default="")
+    # parser.add_argument("--destination", "-d", help="New path.", default="")
 
     args = parser.parse_args()
     path = P(args.path).expanduser().absolute()
 
-    if system == "Windows":  # move files over to WSL
-        # the following works for files and folders alike.
-        path.copy(folder=WSL_FROM_WIN.joinpath(UserName).joinpath(path.rel2home().parent), overwrite=True)
-    else:  # move files from WSL to win
-        path.copy(folder=WIN_FROM_WSL.joinpath(UserName).joinpath(path.rel2home().parent), overwrite=True)
+    if args.same_file_system:
+        if system == "Windows":  # move files over to WSL
+            path.copy(folder=WSL_FROM_WIN.joinpath(UserName).joinpath(path.rel2home().parent), overwrite=True)  # the following works for files and folders alike.
+        else:  # move files from WSL to win
+            path.copy(folder=WIN_FROM_WSL.joinpath(UserName).joinpath(path.rel2home().parent), overwrite=True)
+    else:
+        from crocodile.meta import SSH
+        ssh = SSH()
+        ssh.copy_from_here(source=path)
 
 
 if __name__ == '__main__':
