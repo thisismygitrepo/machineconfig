2081b42f1acfbf58b53d3efbd8b04f23a69df670
Alex Al-Saffar <programmer@usa.com>
2022-12-04 13:06:47 +1030

    use same ssh keys for win and wsl

--- src/machineconfig/scripts/python/transfer_wsl_win.py
+++ src/machineconfig/scripts/python/transfer_wsl_win.py
@@ -30,9 +30,7 @@ Otherwise, a flag must be raised to indicate the direction.""")
             path.copy(folder=WIN_FROM_WSL.joinpath(UserName).joinpath(path.rel2home().parent), overwrite=True)
     else:
         from crocodile.meta import SSH
-        sshkey = args.sshkey
-        if args.sshkey is None: sshkey = P.home().joinpath(".ssh").search("*.pub").apply(lambda x: x.with_name(x.stem)); sshkey = sshkey[0] if len(sshkey) else None
-        ssh = SSH(port=args.port, sshkey=sshkey)
+        ssh = SSH(port=args.port, sshkey=args.sshkey)
         ssh.copy_from_here(source=path)
 
 
