e7a53998c3bcf9e7eae1a63ff8a1b0a17d95eb35
Alex Al-Saffar <programmer@usa.com>
2022-12-04 13:11:40 +1030

    fixed a bug

--- src/machineconfig/scripts/python/devops.py
+++ src/machineconfig/scripts/python/devops.py
@@ -121,7 +121,7 @@ def main():
 
         if direction == "BACKUP" and drive == "OneDrive":
             from machineconfig.scripts.python.bu_onedrive_sx import main
-            for item in items: main(which="default", zip_first=item['zip'], encrypt_first=item['encrypt'], file=tb.P(item['path']).expanduser(), key=None, pwd=None)
+            for item in items: main(which="default", zip_first=item['zip'], encrypt_first=item['encrypt'], file=tb.P(item['path']).expanduser(), key=None, pwd=None, overwrite=True)
         elif direction == "RETRIEVE" and drive == "OneDrive":
             from machineconfig.scripts.python.bu_onedrive_rx import main
             for item in items: main(which="default", unzip=item['zip'], decrypt=item['encrypt'], file=tb.P(item['path']).expanduser(), overwrite=True, key=None, pwd=None)
