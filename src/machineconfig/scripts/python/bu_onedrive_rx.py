c749403b194057c0facf2bd93238f480452c4ef7
Alex Al-Saffar <programmer@usa.com>
2022-12-06 10:06:20 +1030

    added Onedrive retrieval gaurd

--- src/machineconfig/scripts/python/bu_onedrive_rx.py
+++ src/machineconfig/scripts/python/bu_onedrive_rx.py
@@ -1,9 +1,11 @@
 
 # from crocodile.environment import OneDriveConsumer, OneDriveCommercial
+from crocodile.environment import OneDriveExe
 import crocodile.toolbox as tb
 import argparse
 from crocodile.comms.helper_funcs import process_retrieved_file
 import os
+import time
 
 
 def main(file, unzip, decrypt, which, overwrite, key, pwd):
@@ -18,6 +20,16 @@ def main(file, unzip, decrypt, which, overwrite, key, pwd):
     if unzip and decrypt: source_file = source_file + ".zip.enc"
 
     print(f"Retriveing `{source_file}` \nTo `{target_file.parent}` folder.")
+    if source_file.exists(): pass
+    else:
+        OneDriveExe()
+        trials = 5
+        while trials > 0 and not source_file.exists():
+            trials -= 1
+            print(f"Source file doesn't exist, waiting for OneDrive to sync ...")
+            time.sleep(3)
+    if not source_file.exists(): raise FileNotFoundError(f"Could not read the file on the could.")
+            
     tmp_file = source_file.copy(folder=target_file.parent, overwrite=overwrite)  # make sure to avoid doing decryption in the storage site.
     process_retrieved_file(tmp_file, decrypt=decrypt, unzip=unzip, key=key, pwd=pwd)
 
