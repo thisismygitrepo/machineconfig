0b4beade223ce1518fddedf3f3e52cceb479e164
Alex Al-Saffar <programmer@usa.com>
2022-12-06 09:16:59 +1030

    added print statement to backup and retrieve

--- src/machineconfig/jobs/python_generic_installers/delta.py
+++ src/machineconfig/jobs/python_generic_installers/delta.py
@@ -4,7 +4,24 @@ from platform import system
 import crocodile.toolbox as tb
 
 repo_url = tb.P(r"https://github.com/dandavison/delta")
+# from https://github.com/dandavison/delta#configuration
+config = """
+[core]
+    pager = delta
 
+[interactive]
+    diffFilter = delta --color-only
+
+[delta]
+    navigate = true    # use n and N to move between diff sections
+    light = false      # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
+
+[merge]
+    conflictstyle = diff3
+
+[diff]
+    colorMoved = default
+"""
 
 def main():
     if system() == 'Windows':
@@ -17,8 +34,12 @@ def main():
         path = release.joinpath(f"delta-{str(release[-1]).replace('v', '')}-x86_64-unknown-linux-gnu.tar.gz").download()
         downloaded = path.ungz_untar(inplace=True)
         _ = find_move_delete_linux(downloaded, "delta", delete=True)
+    txt = tb.P("~/.gitconfig").read_text()
+    if config in text: pass
+    else: tb.P("~/.gitconfig").write_text(txt + "\n" + config)
 
 
 if __name__ == '__main__':
     main()
     # pass
+
