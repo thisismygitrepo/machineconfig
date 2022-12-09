e38862f7c9e918971e384f2fe91213bb5ca65878
Alex Al-Saffar <programmer@usa.com>
2022-12-09 17:08:25 +1030

    added features to update repos

--- src/machineconfig/jobs/windows/update_essentials.ps1
+++ src/machineconfig/jobs/windows/update_essentials.ps1
@@ -1,7 +1,14 @@
 
 $orig_path = $PWD
+
+# updateBegins
+
 cd ~/code/crocodile
 git pull origin # --git-dir $env:USERPROFILE/code/crocodile/.git
+
 cd ~/code/machineconfig
 git pull origin # --git-dir $env:USERPROFILE/code/machineconfig/.git
+
+# updateEnds
+
 cd $orig_path
--- src/machineconfig/scripts/python/devops.py
+++ src/machineconfig/scripts/python/devops.py
@@ -61,12 +61,48 @@ def main():
             if program is None: program = "echo 'Finished Installation'"  # write an empty program
 
     elif choice_key == "UPDATE essential repos":
-        program_windows = f"{LIBRARY_ROOT}/jobs/windows/update_essentials.ps1"
-        program_linux = f"source {LIBRARY_ROOT}/jobs/linux/update_essentials"
-        if tb.P.home().joinpath("code/crocodile").expanduser().exists():
-            program = program_linux if system() == "Linux" else program_windows
-        else:
-            program = "pip install --upgrade crocodile; pip install --upgrade machineconfig"
+        repos = tb.P.home().joinpath("dotfiles/config/repos.ini")
+        local_install_repos = []
+        global_packages = []
+
+        if repos.exists():
+            repos = repos.readit()
+            repo_package_list = list(repos.keys())
+            repo_package_list.remove("DEFAULT")
+        else: repo_package_list = ["crocodile", "machineconfig"]
+
+        for a_package in repo_package_list:
+            try:
+                repo = tb.install_n_import("git", "gitpython").Repo(__import__(a_package).__file__, search_parent_directories=True)
+                local_install_repos.append(repo)
+            except:
+                global_packages.append(a_package)
+
+        sep = "\n"
+        if system() == "Linux":
+            program = tb.P(f"{LIBRARY_ROOT}/jobs/linux/update_essentials").read_text()
+            additions = []
+            for a_repo in local_install_repos:
+                if "machineconfig" in a_repo.working_dir:
+                    an_addition = f"""
+cd "{a_repo.working_dir}"
+git reset --hard
+git pull origin
+chmod +x ~/scripts -R
+chmod +x ~/code/machineconfig/src/machineconfig/jobs/linux -R
+chmod +x ~/code/machineconfig/settings/lf_linux/exe -R
+"""
+                    additions.append(an_addition)
+                else:
+                    additions.append(f"""cd "{a_repo.working_dir}"; {sep.join([f'git pull {remote.name}' for remote in a_repo.remotes])}""")
+            addition = "\n".join(additions)
+        elif system() == "Windows":
+            program = tb.P(f"{LIBRARY_ROOT}/jobs/windows/update_essentials.ps1").read_text()
+            addition = "\n".join([f"""cd "{a_repo.working_dir}"; {sep.join([f'git pull {remote.name}' for remote in a_repo.remotes])}""" for a_repo in local_install_repos])
+        else: raise NotImplementedError(f"System {system()} not supported")
+
+        program = program.split("# updateBegins")[0] + addition + program.split("# updateEnds")[1]
+        program += f"\npip install --upgrade {' '.join(global_packages)}\n"
 
     elif choice_key == "DEVAPPS install":
         program_windows = f"{LIBRARY_ROOT}/setup_windows/devapps.ps1"
