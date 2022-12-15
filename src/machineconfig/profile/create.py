11361884e6c26ef42f959bdb9bd26f38b49984b3
Alex Al-Saffar <programmer@usa.com>
2022-12-15 11:16:49 +1030

    fixed PATH problem in windows

--- src/machineconfig/profile/create.py
+++ src/machineconfig/profile/create.py
@@ -48,12 +48,21 @@ def main_symlinks():
     overwrite = True
     exclude = ["autostart_windows"]  # "wsl_linux", "wsl_windows"
 
-    program_keys = list(symlink_mapper.keys())
+    program_keys = list(symlink_mapper.keys()) + ["aws", "ssh"]
     choice = display_options(msg="Which symlink to create?", options=program_keys + ["all"], default="all")
-    if str(choice) == "all": pass
+    if str(choice) == "all":
+        if system == "Windows" and not tb.Terminal.is_user_admin():
+            print("*" * 200)
+            raise RuntimeError(f"Run terminal as admin and try again, otherwise, there will be too many popups for admin requests and no chance to terminate the program.")
     else: program_keys = [choice]
 
     for program_key in program_keys:
+        if program_key == "aws":
+            link_aws(overwrite=overwrite)
+            continue
+        elif program_key == "ssh":
+            link_ssh(overwrite=overwrite)
+            continue
         if program_key in exclude or OTHER_SYSTEM in program_key:
             print(f"Skipping {program_key} for {system}")
             continue
@@ -61,49 +70,56 @@ def main_symlinks():
             try: symlink(this=file_map['this'], to_this=file_map['to_this'].replace("REPO_ROOT", REPO_ROOT.as_posix()).replace("LIBRARY_ROOT", LIBRARY_ROOT.as_posix()), overwrite=overwrite)
             except Exception as ex: print("Config error: ", program_key, file_key, "missing keys 'this ==> to_this'.", ex)
 
-    link_aws(overwrite=overwrite)
-    link_ssh(overwrite=overwrite)
     if system == "Linux": tb.Terminal().run(f'chmod +x {LIBRARY_ROOT.joinpath(f"scripts/{system.lower()}")} -R')
 
 
-def add_to_shell_profile_path(dirs: list):
-    addition = PathVar.append_temporarily(dirs=dirs)
-    if system == "Windows": file = tb.Terminal().run("$profile", shell="pwsh").as_path
-    elif system == "Linux": file = tb.P("~/.bashrc").expanduser()
-    else: raise ValueError
-    file.copy(name=file.name + f".orig_" + tb.randstr())
-    file.modify_text(addition, addition, replace_line=False, notfound_append=True)
+# --------------------------------------- SHELL PROFILE --------------------------------------------------------
+# The following is not a symlink creation, but modification of shell profile by additing dirs to PATH
+# Shell profile is either in dotfiles and is synced (as in Windows), hence no need for update, or is updated on the fly (for Linux)
+# for windows it won't change the profile, if the profile was modified already e.g. due to syncing
 
 
-def main_env_path():
+def get_shell_profile_path():
+    if system == "Windows": profile_path = tb.Terminal().run("$profile", shell="pwsh").as_path
+    elif system == "Linux": profile_path = tb.P("~/.bashrc").expanduser()
+    else: raise ValueError(f"Not implemented for this system {system}")
+    return profile_path
+
+
+def main_env_path(profile_path=None):
     env_path = LIBRARY_ROOT.joinpath("profile/env_path.toml").readit()
-    # The following is not a symlink creation, but modification of shell profile by additing dirs to PATH
-    # Shell profile is either in dotfiles and is synced (as in Windows), hence no need for update, or is updated on the fly (for Linux)
-    # for windows it won't change the profile, if the profile was modified already e.g. due to syncing
-    add_to_shell_profile_path(env_path[f'path_{system.lower()}']['extension'])
+    dirs = env_path[f'path_{system.lower()}']['extension']
+    addition = PathVar.append_temporarily(dirs=dirs)
+    profile_path = profile_path or get_shell_profile_path()
+    profile_path.copy(name=profile_path.name + f".orig_" + tb.randstr())
+    profile_path.modify_text(addition, addition, replace_line=False, notfound_append=True)
 
 
-def main_add_sources_to_shell_profile():
+def main_add_sources_to_shell_profile(profile_path=None):
     sources = LIBRARY_ROOT.joinpath("profile/sources.toml").readit()
+
+    profile_path = profile_path or get_shell_profile_path()
+    profile = profile_path.read_text()
+
     for file in sources[system.lower()]['files']:
         file = file.replace("REPO_ROOT", REPO_ROOT.as_posix()).replace("LIBRARY_ROOT", LIBRARY_ROOT.as_posix())
         file = tb.P(file).collapseuser().as_posix()  # this makes the shell profile interuseable across machines.
-        if system == "Windows": tb.Terminal().run("$profile", shell="pwsh").as_path.modify_text(f". {file}", f". {file}", replace_line=True, notfound_append=True)
-        elif system == "Linux": tb.P("~/.bashrc").expanduser().modify_text(f"source {file}", f"source {file}", notfound_append=True)
-        else: raise ValueError
+        if file in profile:
+            if system == "Windows": profile += f". {file}"
+            elif system == "Linux": profile += f"source {file}"
+            else: raise ValueError(f"Not implemented for this system {system}")
+        else: print(f"SKIPPING source `{file}`, it is already sourced in shell profile.")
+    profile_path.write_text(profile)
 
 
-def main_add_patches_to_shell_profile():
+def main_add_patches_to_shell_profile(profile_path=None):
     patches = LIBRARY_ROOT.joinpath(f"profile/patches/{system.lower()}").search()
 
     choice = display_options(msg="Which patch to add?", options=patches.list + ["all"], default="all")
     if str(choice) == "all": patches = patches
     else: patches = [choice]
 
-    if system == "Windows":
-        profile_path = tb.Terminal().run("$profile", shell="pwsh").as_path
-    elif system == "Linux": profile_path = tb.P("~/.bashrc")
-    else: raise ValueError
+    profile_path = profile_path or get_shell_profile_path()
     profile = profile_path.read_text()
 
     for patch_path in patches:
--- src/machineconfig/profile/patches/windows/zoxide.ps1
+++ src/machineconfig/profile/patches/windows/zoxide.ps1
@@ -1,5 +1,5 @@
 
-# from https://github.com/ajeetdsouza/zoxide
+# patched by machineconfig from https://github.com/ajeetdsouza/zoxide
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
