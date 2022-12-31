d77722c6e71b5b8a20b995c55159a699c4cb91a1
Alex Al-Saffar <programmer@usa.com>
2022-12-31 19:57:52 +1030

    minor SSH changes

--- src/machineconfig/setup_windows/openssh_all.ps1
+++ src/machineconfig/setup_windows/openssh_all.ps1
@@ -6,6 +6,7 @@ if ($my_keys -eq $null) {
     $my_keys="https://github.com/thisismygitrepo.keys"
 }
 cd ~
+echo $null >> .ssh/authorized_keys  # powershell way of touching a file if it doesn't exist
 (Invoke-WebRequest $my_keys).Content >> .ssh/authorized_keys
-(Invoke-WebRequest $my_keys).Content >> .ssh/id_rsa.pub
+(Invoke-WebRequest $my_keys).Content > .ssh/id_rsa.pub
 Invoke-WebRequest https://raw.githubusercontent.com/thisismygitrepo/machineconfig/main/src/machineconfig/setup_windows/openssh-server_add-sshkey.ps1 | Invoke-Expression
