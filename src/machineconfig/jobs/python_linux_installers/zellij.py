b992959e879e9de57b76aa0ac510a610c8122ff9
Alex Al-Saffar <programmer@usa.com>
2023-01-01 20:16:10 +1030

    added z_ls

--- src/machineconfig/jobs/python_linux_installers/zellij.py
+++ src/machineconfig/jobs/python_linux_installers/zellij.py
@@ -3,11 +3,22 @@
 from machineconfig.utils.utils import get_latest_release, find_move_delete_linux
 import crocodile.toolbox as tb
 
+bash_integration = """
+
+# adopted from https://zellij.dev/documentation/integration.html
+eval "$(zellij setup --generate-auto-start bash)"
+
+"""
 
 def main():
     # url = get_latest_release("https://github.com/zellij-org/zellij")
     download = tb.P(f"https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz").download().ungz_untar(inplace=True)
     find_move_delete_linux(downloaded=download, tool_name="zellij")
+
+    profile_path = tb.P.home().joinpath(".bashrc")
+    profile_text = profile_path.read_text()
+    if bash_integration not in profile_text:
+        profile_path.write_text(profile_text + bash_integration)
 
 
 if __name__ == '__main__':
