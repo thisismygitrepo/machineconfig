7e59f95eb05df93b6da22f02d28dc4d5d062e4bd
Alex Al-Saffar <programmer@usa.com>
2023-01-13 14:39:39 +1030

    activate_ve now remembers its default value.

--- src/machineconfig/setup_linux/symlinks.sh
+++ src/machineconfig/setup_linux/symlinks.sh
@@ -4,6 +4,6 @@
 #source ~/venvs/$ve_name/bin/activate || exit
 source ~/code/machineconfig/src/machineconfig/scripts/linux/activate_ve
 # TODO: find a way to get path of activate_ve in more dynamic way that doesn't hardcode path to machineconfig.
-python -m fire machineconfig.profile.create main
+python -m fire machineconfig.profile.create main --program_name=all
 . ~/.bashrc
 deactivate
--- src/machineconfig/setup_windows/symlinks.ps1
+++ src/machineconfig/setup_windows/symlinks.ps1
@@ -1,6 +1,6 @@
 
 #$ve_name='ve'
 #& ~/venvs/$ve_name/Scripts/Activate.ps1
-. ~/code/machineconfig/
-python -m fire machineconfig.profile.create main
+. ~/code/machineconfig/src/machineconfig/scripts/windows/activate_ve.ps1
+python -m fire machineconfig.profile.create main --program_name=all
 deactivate
