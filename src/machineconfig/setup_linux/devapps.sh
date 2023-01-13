cf496e05c4a46ab17475444cb15bfed202313ea0
Alex Al-Saffar <programmer@usa.com>
2023-01-13 15:00:15 +1030

    fixed a bug

--- src/machineconfig/setup_linux/devapps.sh
+++ src/machineconfig/setup_linux/devapps.sh
@@ -3,6 +3,6 @@
 #ve_name='ve'
 #source ~/venvs/$ve_name/bin/activate || exit
 source ~/scripts/activate_ve
-python -m fire machineconfig.jobs.python.python_linux_installers_all
+python -m fire machineconfig.jobs.python.python_linux_installers_all main  # this installs everything.
 . ~/.bashrc
 deactivate
--- src/machineconfig/setup_windows/devapps.ps1
+++ src/machineconfig/setup_windows/devapps.ps1
@@ -1,5 +1,5 @@
 
 $ve_name='ve'
 & ~/venvs/$ve_name/Scripts/Activate.ps1
-python -m fire machineconfig.jobs.python.python_windows_installers_all
+python -m fire machineconfig.jobs.python.python_windows_installers_all main  # this installs everything.
 deactivate
