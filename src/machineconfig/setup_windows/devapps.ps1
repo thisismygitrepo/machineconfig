9e0bc78d66dfdd59876af04c70f90d4ad0d99a50
Alex Al-Saffar <programmer@usa.com>
2023-01-13 14:57:23 +1030

    fixed minor bugs

--- src/machineconfig/setup_windows/devapps.ps1
+++ src/machineconfig/setup_windows/devapps.ps1
@@ -1,5 +1,5 @@
 
 $ve_name='ve'
 & ~/venvs/$ve_name/Scripts/Activate.ps1
-python -m fire machineconfig.jobs.python.python_windows_installers_all main --program_name=all
+python -m fire machineconfig.jobs.python.python_windows_installers_all
 deactivate
