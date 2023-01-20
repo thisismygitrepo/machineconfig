9c8f54e87c0823b31a633aacb4f8131361d8c18f
Alex Al-Saffar <programmer@usa.com>
2023-01-20 17:11:42 +1030

    fixed linux access isssue with .ssh

--- src/machineconfig/profile/create.py
+++ src/machineconfig/profile/create.py
@@ -27,8 +27,8 @@ def link_ssh(overwrite=True):
         symlink(path.joinpath(item.name), item, overwrite=overwrite)
     if system == "Linux":  # permissions of ~/dotfiles/.ssh should be adjusted
         try:
-            subprocess.run("chmod 700 ~/dotfiles/.ssh/")  # may require sudo
-            subprocess.run("chmod 600 ~/dotfiles/.ssh/*")
+            subprocess.run(f"chmod 700 {target.collapseuser().as_posix()}/")  # may require sudo
+            subprocess.run(f"chmod 600 {target.collapseuser().as_posix()}/*")
         except Exception as e:
             ERROR_LIST.append(e)
             print("Caught error", e)
