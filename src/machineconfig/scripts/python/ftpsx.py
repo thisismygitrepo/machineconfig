e47f69eee59fadbcb37c72b71d976d6cb7d70942
Alex Al-Saffar <programmer@usa.com>
2023-02-08 10:01:18 +1030

    fixed a bug

--- src/machineconfig/scripts/python/ftpsx.py
+++ src/machineconfig/scripts/python/ftpsx.py
@@ -26,7 +26,7 @@ def main():
         print(f"Caution: Ensure that username is passed appropriately as this exception only handles password.")
         import getpass
         pwd = getpass.getpass()
-        ssh = SSH(rf'{args.machine}', password=pwd)
+        ssh = SSH(rf'{args.machine}', pwd=pwd)
 
     ssh.copy_from_here(source=args.file, target=args.destination, z=args.zipFirst, r=args.recursive)
     # ssh.print_summary()
