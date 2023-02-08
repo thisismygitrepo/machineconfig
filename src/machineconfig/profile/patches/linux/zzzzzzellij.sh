b6e2ea9a63d9e173e004f272b56d96da88545433
Alex Al-Saffar <programmer@usa.com>
2023-02-08 19:22:33 +1030

    added standard layout

--- src/machineconfig/profile/patches/linux/zzzzzzellij.sh
+++ src/machineconfig/profile/patches/linux/zzzzzzellij.sh
@@ -1,4 +1,9 @@
 
+# the patch is caleld zzzzzzellij.sh to make sure it is the last one to be sourced at the end of the .bashrc
 # adopted from https://zellij.dev/documentation/integration.html
 # eval "$(zellij setup --generate-auto-start bash)"
-z_ls  # make sure it is towards the end of the script.
+if [[ $(zellij list-sessions) != *"(current)"* ]]; then
+  z_ls
+# this statement prevents running z_ls script which is already not going to do anything except echoing that its already inside a session and will therefore not do anything.
+# until programmatic detach functionality is implemented in zellij, this script will continue to not do anything if run inside a session.
+fi
