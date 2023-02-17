7bae7dffb593cdd8b4ab5c05a8c6cf57aee8dd24
Alex Al-Saffar <programmer@usa.com>
2023-02-17 19:58:22 +1030

    added generic os flag to cloud utility

--- src/machineconfig/scripts/python/cloud_rx.py
+++ src/machineconfig/scripts/python/cloud_rx.py
@@ -17,12 +17,13 @@ def arg_parser():
     # optional argument
     parser.add_argument("--remote_dir", "-d", help="Remote directory to send to.", default="")
     parser.add_argument("--relative_to_home", "-r", help="Relative to `myhome` folder", action="store_true")  # default is False
+    parser.add_argument("--generic_os", "-g", help="Generic OS path (relevant only when relative flag is raised as well.", action="store_true")
     parser.add_argument("--key", "-k", help="Key for encryption", default=None)
     parser.add_argument("--pwd", "-p", help="Password for encryption", default=None)
 
     args = parser.parse_args()
     tb.P(args.file).from_cloud(cloud=args.cloud, unzip=args.unzip, decrypt=args.decrypt, overwrite=args.overwrite, pwd=args.pwd, key=args.key,
-                               rel2home=args.relative_to_home)
+                               rel2home=args.relative_to_home, generic_os=args.generic_os,)
 
 
 if __name__ == "__main__":
