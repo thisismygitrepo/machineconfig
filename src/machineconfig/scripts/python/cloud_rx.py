dde84a9c359b2b838ec7e7ce6e4d380efbb14a92
Alex Al-Saffar <programmer@usa.com>
2023-02-17 22:49:34 +1030

    refined to_cloud

--- src/machineconfig/scripts/python/cloud_rx.py
+++ src/machineconfig/scripts/python/cloud_rx.py
@@ -17,14 +17,14 @@ def arg_parser():
     # optional argument
     parser.add_argument("--remote_dir", "-d", help="Remote directory to send to.", default="")
     parser.add_argument("--relative_to_home", "-r", help="Relative to `myhome` folder", action="store_true")  # default is False
-    parser.add_argument("--generic_os", "-g", help="Generic OS path (relevant only when relative flag is raised as well.", action="store_true")
+    parser.add_argument("--os_specific", "-o", help="OS specific path (relevant only when relative flag is raised as well.", action="store_true")
     parser.add_argument("--key", "-k", help="Key for encryption", default=None)
     parser.add_argument("--pwd", "-p", help="Password for encryption", default=None)
 
     args = parser.parse_args()
     tb.P(args.file).from_cloud(cloud=args.cloud, unzip=args.unzip, decrypt=args.decrypt, overwrite=args.overwrite,
                                pwd=args.pwd, key=args.key,
-                               rel2home=args.relative_to_home, generic_os=args.generic_os,)
+                               rel2home=args.relative_to_home, os_specific=args.os_specific,)
 
 
 if __name__ == "__main__":
