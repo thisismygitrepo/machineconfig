048496680ce0b8dfbc70e57d100403b848c925df
Alex Al-Saffar <programmer@usa.com>
2023-02-26 15:05:22 +1030

    added cloud_sync to linux

---
+++ src/machineconfig/profile/fix_shell_profiles.py
@@ -0,0 +1,8 @@
+
+# Non-login shells, .bashrc > .profile are read.
+# for login shells, e.g. SSH, .bash_login > .bash_profile > .profile is read.
+
+# The strategy to ensure consistent configuration for login and nonlogin shells is to put all the configuration in .bashrc, and then source it from .bash_profile and .profile.
+
+# ensure .bash_profile only calls .profile
+# ensure .profile calls .bashrc only at the end.
