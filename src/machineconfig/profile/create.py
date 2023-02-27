09a6b7a0475dd898be1907326ebccc1a868aec61
Alex Al-Saffar <programmer@usa.com>
2023-02-27 14:44:58 +1030

    fixed path

--- src/machineconfig/profile/create.py
+++ src/machineconfig/profile/create.py
@@ -130,7 +130,8 @@ def main_add_sources_to_shell_profile(profile_path=None, choice=None):
 
     for a_file in sources:
         file = a_file.replace("REPO_ROOT", REPO_ROOT.as_posix()).replace("LIBRARY_ROOT", LIBRARY_ROOT.as_posix())
-        file = tb.P(file).collapseuser().as_posix()  # this makes the shell profile interuseable across machines.
+        file = tb.P(file).collapseuser()  # this makes the shell profile interuseable across machines.
+        file = file.as_posix() if system == "Linux" else str(file)
         if file not in profile:
             if system == "Windows": profile += f"\n. {file}"
             elif system == "Linux": profile += f"\nsource {file}"
--- src/machineconfig/scripts/python/cloud_sync.py
+++ src/machineconfig/scripts/python/cloud_sync.py
@@ -10,11 +10,13 @@ def args_parser():
 
     parser.add_argument("--cloud", "-c", help="rclone cloud profile name.", default=None)
     parser.add_argument("--path", "-p", help="rclone cloud profile name.", default=None)
+    parser.add_argument("--transfers", "-t", help="Number of threads in syncing.", default=10)  # default is False
     # parser.add_argument("--key", "-k", help="Key for encryption", default=None)
     # parser.add_argument("--pwd", "-P", help="Password for encryption", default=None)
     parser.add_argument("--upload", "-u", help="Upload direction, local is unchanged.", action="store_true")  # default is False
     parser.add_argument("--download", "-d", help="Download direction, local is unchanged.", action="store_true")  # default is False
     parser.add_argument("--bisync", "-b", help="Bidirectional sync.", action="store_true")  # default is False
+    parser.add_argument("--delete", "-D", help="Delete files in remote that are not in local.", action="store_true")  # default is False
 
     parser.add_argument("--verbose", "-v", help="Verbosity of mprocs to show details of syncing.", action="store_true")  # default is False
 
@@ -30,10 +32,14 @@ def args_parser():
     remote = "myhome/generic_os" / local.rel2home()
     print(f"Syncing {local} to {args.cloud}:{remote} | using strategy `{strategy}`")
 
-    if args.upload: rclone_cmd = f"""rclone sync {local} {args.cloud}:{remote} --progress"""
-    elif args.download: rclone_cmd = f"""rclone sync {args.cloud}:{remote} {local} --progress"""
-    elif args.bisync: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote} --resync --progress"""
-    else: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote} --resync --progress"""
+    if args.upload: rclone_cmd = f"""rclone sync {local} {args.cloud}:{remote}"""
+    elif args.download: rclone_cmd = f"""rclone sync {args.cloud}:{remote} {local}"""
+    elif args.bisync: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote}"""
+    else: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote}"""
+    rclone_cmd += f" --progress --transfers={args.transfers}"
+    if args.bisync: rclone_cmd += " --resync"
+    if args.delete: rclone_cmd += " --delete-during"
+    # if args.verbose: rclone_cmd += " --verbose"
 
     if args.verbose: txt = get_mprocs_mount_txt(cloud=args.cloud, cloud_brand="unknown", rclone_cmd=rclone_cmd, localpath=local)
     else: txt = f"""cd ~\n{rclone_cmd}"""
