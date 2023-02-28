cca52367118ae4d83ecc6365037c919400188330
Alex Al-Saffar <programmer@usa.com>
2023-03-01 10:21:18 +1030

    made cloud_sync more flexible

--- src/machineconfig/scripts/python/cloud_sync.py
+++ src/machineconfig/scripts/python/cloud_sync.py
@@ -9,7 +9,7 @@ def args_parser():
     parser = argparse.ArgumentParser(description="Secure Repo CLI.")
 
     parser.add_argument("--cloud", "-c", help="rclone cloud profile name.", default=None)
-    parser.add_argument("--path", "-p", help="rclone cloud profile name.", default=None)
+    parser.add_argument("--localpath", "-l", help="rclone cloud profile name.", default=None)
     parser.add_argument("--transfers", "-t", help="Number of threads in syncing.", default=10)  # default is False
     # parser.add_argument("--key", "-k", help="Key for encryption", default=None)
     # parser.add_argument("--pwd", "-P", help="Password for encryption", default=None)
@@ -22,20 +22,21 @@ def args_parser():
 
     args = parser.parse_args()
 
-    # map short flags to long flags (-u -> --upload), for easier use in the script
-    if args.upload: strategy = "upload"
-    elif args.download: strategy = "download"
-    elif args.bisync: strategy = "bisync"
-    else: strategy = "bisync"
-
     local = P(args.path).expanduser().absolute()
-    remote = "myhome/generic_os" / local.rel2home()
-    print(f"Syncing {local} to {args.cloud}:{remote} | using strategy `{strategy}`")
+    if ":" not in args.cloud:  # usee did not specify remotepath, so it will be inferred here:
+        remote = "myhome/generic_os" / local.rel2home()
+        remote_exp = f"{args.cloud}:{remote}"
+    else: remote_exp = args.cloud
 
-    if args.upload: rclone_cmd = f"""rclone sync {local} {args.cloud}:{remote}"""
-    elif args.download: rclone_cmd = f"""rclone sync {args.cloud}:{remote} {local}"""
-    elif args.bisync: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote}"""
-    else: rclone_cmd = f"""rclone bisync {local} {args.cloud}:{remote}"""
+    # map short flags to long flags (-u -> --upload), for easier use in the script
+    if args.upload: _strategy = "upload"; print(f"Syncing {local} {'>' * 30} {remote_exp}`")
+    elif args.download: _strategy = "download"; print(f"Syncing {local} {'<' * 30} {remote_exp}`")
+    else: _strategy = "bisync"; print(f"Syncing {local} {'<>' * 15} {remote_exp}`")
+
+    if args.upload: rclone_cmd = f"""rclone sync {local} {remote_exp}"""
+    elif args.download: rclone_cmd = f"""rclone sync {remote_exp} {local}"""
+    elif args.bisync: rclone_cmd = f"""rclone bisync {local} {remote_exp}"""
+    else: rclone_cmd = f"""rclone bisync {local} {remote_exp}"""
     rclone_cmd += f" --progress --transfers={args.transfers}"
     if args.bisync: rclone_cmd += " --resync"
     if args.delete: rclone_cmd += " --delete-during"
