698e30b196c137556e062fab07dea935df371552
Alex Al-Saffar <programmer@usa.com>
2023-03-20 10:23:56 +1030

    added all option to repos

--- src/machineconfig/scripts/python/repos.py
+++ src/machineconfig/scripts/python/repos.py
@@ -11,7 +11,6 @@ tm = tb.Terminal()
 
 def commit_one(path, mess="auto_commit_" + tb.randstr()):
     return f'''
-echo "{("Committing " + str(path)).center(80, "-")}"
 cd {path}; git add .; git commit -am "{mess}"
 echo ""
 echo ""
@@ -25,7 +24,6 @@ def push_one(path):
     for remote in remotes:
         if remote != "": cmds.append(f'cd {path}; git push {remote}')
     return f"""
-echo "{("Pushing " + str(path)).center(80, "-")}"
 {join.join(cmds)}
 echo ""
 echo ""
@@ -39,38 +37,37 @@ def pull_one(path):
     for remote in remotes:
         if remote != "": cmds.append(f'cd {path}; git pull {remote}')
     return f"""
-echo "{("Pulling " + str(path)).center(80, "-")}"
 {join.join(cmds)}
 echo ""
 echo ""
 """
 
 
-def commit_all(path=None):
-    q = path.search("*").apply(lambda x: commit_one(x))  # reduce(lambda x, y: x + "\n\n" + y)[0]
-    return "\n\n".join(q)
-def push_all(path=None): return "\n\n".join(path.search("*").apply(lambda x: push_one(x)))
-def pull_all(path=None): return "\n\n".join(path.search("*").apply(lambda x: pull_one(x)))
-
-
 def main():
     parser = argparse.ArgumentParser(description='REPO MANAGER')
     # POSITIONAL
     parser.add_argument("directory", help="folder containing repos.", default="")
-    # # FLAGS
+    # FLAGS
     parser.add_argument("--push", help=f"push", action="store_true")
     parser.add_argument("--pull", help=f"pull", action="store_true")
     parser.add_argument("--commit", help=f"commit", action="store_true")
+    parser.add_argument("--all", help=f"all", action="store_true")
 
     args = parser.parse_args()
     if args.directory == "": path = tb.P.home().joinpath("code")
     else: path = tb.P(args.directory).expanduser().absolute()
 
-    if args.commit: program = commit_all(path)
-    elif args.push: program = push_all(path)
-    elif args.pull: program = pull_all(path)
-    else: program = "echo 'no action specified, try to pass --push, --pull, or --commit'"
+    program = ""
+    for a_path in path.search("*"):
+        program += f"""echo "{("Handling " + str(path)).center(80, "-")}" """
+        if args.pull or args.all: program += f"""echo 'Pulling'\n""" + pull_one(a_path) + "\n"
+        elif args.commit or args.all: program += f"""echo 'Committing'\n""" + commit_one(a_path) + "\n"
+        elif args.push or args.all: program += f"""echo 'Pushing'\n""" + push_one(a_path) + "\n"
+        else:
+            program = "echo 'no action specified, try to pass --push, --pull, --commit or --all'"
+            break
 
+    program = "$orig_path = $pwd\n" + program + "\ncd $orig_path"
     print(f"Executing {PROGRAM_PATH}")
     if system() == 'Windows': PROGRAM_PATH.create(parents_only=True).write_text(program)
     else: PROGRAM_PATH.create(parents_only=True).write_text(f"{program}")
