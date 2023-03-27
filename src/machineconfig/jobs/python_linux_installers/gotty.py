92808abd08b0623175859598a6d304a74f7c2cf5
Alex Al-Saffar <programmer@usa.com>
2023-03-28 07:55:39 +1030

    added gotty installer

---
+++ src/machineconfig/jobs/python_linux_installers/gotty.py
@@ -0,0 +1,15 @@
+
+from machineconfig.utils.utils import get_latest_release, find_move_delete_linux
+import crocodile.toolbox as tb
+
+
+url = r'https://github.com/sorenisanerd/gotty'
+suffix = 'linux_amd64'
+
+
+def main(version=None):
+    release = get_latest_release(url, suffix=suffix, version=version, download_n_extract=True, linux=True, compression="tar.gz")
+
+
+if __name__ == '__main__':
+    main()
