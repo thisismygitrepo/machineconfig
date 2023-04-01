8f101cc33bf127544a1d591a0e9b51ebf4d9b2fd
Alex Al-Saffar <programmer@usa.com>
2023-04-01 13:43:00 +1030

    refactor(opencommit.py): convert opencommit.py to a Python module with a main function feat(opencommit.py): add optional version argument to main function feat(opencommit.py): prompt user for OpenAI API key input in main function docs(opencommit.py): add comments to main function explaining its purpose and usage

--- src/machineconfig/jobs/python_generic_installers/dev/opencommit.py
+++ src/machineconfig/jobs/python_generic_installers/dev/opencommit.py
@@ -2,13 +2,20 @@
 # from machineconfig.utils.utils import write_shell_script
 import crocodile.toolbox as tb
 
-token = input("Enter your OpenAI API key: ")
 
-program = f"""
-# as per: https://github.com/di-sukharev/opencommit
-npm install -g opencommit
-cd ~
-echo "OPENAI_API_KEY={token}" > .opencommit
-"""
+def main(version=None):
+    _ = version
+    token = input("Enter your OpenAI API key: ")
 
-tb.Terminal().run(program, shell="powershell")
+    program = f"""
+    # as per: https://github.com/di-sukharev/opencommit
+    npm install -g opencommit
+    cd ~
+    echo "OPENAI_API_KEY={token}" > .opencommit
+    """
+
+    tb.Terminal().run(program, shell="powershell")
+
+
+if __name__ == '__main__':
+    main()
