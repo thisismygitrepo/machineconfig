bed80d499dd5f3eb3441d1d6e318db9e0d059adf
Alex Al-Saffar <programmer@usa.com>
2023-04-02 16:01:04 +0930

    refactor(launch_multiple_machines): remove unnecessary argument from python command refactor(create_zellij_template.py): remove unused variable and pass function call to main block

--- src/machineconfig/jobs/python/create_zellij_template.py
+++ src/machineconfig/jobs/python/create_zellij_template.py
@@ -55,8 +55,8 @@ def launch_from_ssh_config():
     hosts = display_options(msg="", options=choices, multi=True, fzf=True)
     res = build_template(hosts)
     # write_shell_script(res, execute=False, desc="zellij launch script")
-    return c
+    return None
 
 
 if __name__ == '__main__':
-    pass
+    launch_from_ssh_config()
