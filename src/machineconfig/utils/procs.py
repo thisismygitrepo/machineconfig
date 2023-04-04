29a2303fd3bb09004d10eefddaf6647ab526f0b9
Alex Al-Saffar <programmer@usa.com>
2023-04-04 15:32:12 +0930

    feat(procs.py): add timezone conversion to process create time in ProcessManager class and kill process print statements. Add main guard to file.

--- src/machineconfig/utils/procs.py
+++ src/machineconfig/utils/procs.py
@@ -1,6 +1,7 @@
 
 import psutil
 import pandas as pd
+from pytz import timezone
 
 
 class ProcessManager:
@@ -13,7 +14,7 @@ class ProcessManager:
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess): pass
         df = pd.DataFrame(process_info)
         df.columns = ['pid', 'name', 'username', 'cpu_percent', 'memory_usage_mb', 'status', 'create_time']
-        df['create_time'] = pd.to_datetime(df['create_time'], unit='s')
+        df['create_time'] = pd.to_datetime(df['create_time'], unit='s', utc=True).apply(lambda x: x.tz_convert(timezone('Australia/Adelaide')))
         df = df.sort_values(by='memory_usage_mb', ascending=False)
         self.df = df
 
@@ -31,13 +32,17 @@ class ProcessManager:
             if len(rows) > 0:
                 for pid in rows['pid'].values:
                     psutil.Process(pid).kill()
-                    print(f'Killed process {name} with pid {pid}. It lived {pd.Timestamp.now() - pd.to_datetime(rows["create_time"].values[0], unit="s")}.')
+                    print(f'Killed process {name} with pid {pid}. It lived {pd.Timestamp.now() - pd.to_datetime(rows["create_time"].values[0], unit="s", utc=True).tz_convert(timezone("Australia/Adelaide"))}.')
             else:
                 print(f'No process named {name} found')
         for pid in pids:
             try:
                 proc = psutil.Process(pid)
                 proc.kill()
-                print(f'Killed process with pid {pid} and name {proc.name()}.  It lived {pd.Timestamp.now() - pd.to_datetime(proc.create_time(), unit="s")}.')
+                print(f'Killed process with pid {pid} and name {proc.name()}.  It lived {pd.Timestamp.now() - pd.to_datetime(proc.create_time(), unit="s", utc=True).tz_convert(timezone("Australia/Adelaide"))}.')
             except psutil.NoSuchProcess:
                 print(f'No process with pid {pid} found')
+
+
+if __name__ == '__main__':
+    pass
