d501ff31150edf1f9d4fa1cebb4168c1c0629bfc
Alex Al-Saffar <alex.al-saffar@sa.gov.au>
2023-07-03 22:23:47 +0930

    fix(mount_nfs.py): add support for interactive choice of share path fix(mount_nfs.py): add support for entering custom local mount path

--- src/machineconfig/scripts/python/mount_nfs.py
+++ src/machineconfig/scripts/python/mount_nfs.py
@@ -1,10 +1,13 @@
 
 import crocodile.toolbox as tb
-from machineconfig.utils.utils import display_options, PROGRAM_PATH
-
+from machineconfig.utils.utils import display_options, PROGRAM_PATH, choose_ssh_host
 
 def main():
-    share_path = input("share path? (e.g. machine:/home/alex) = ")
+    share_path = input("share path? (e.g. machine:/home/alex) [press enter for interactive choice] = ")
+    if share_path == "":
+        ssh = tb.SSH(choose_ssh_host())
+        share_path = display_options("Choose a share path: ", options=tb.L(ssh.run("cat /etc/exports").op.split("\n")).filter(lambda x: not x.startswith("#")).apply(lambda x: f"{ssh.hostname}:{x.split(' ')[0]}").list)
+
     mount_path_1 = tb.P(share_path.split(":")[-1])
     remote_server = share_path.split(":")[0]
 
@@ -15,7 +18,7 @@ def main():
         mount_path_3 = tb.P.home().joinpath(f"mounts/{remote_server}/{mount_path_1.name}")
 
     local_mount_point = display_options(msg="mount path", options=[mount_path_1, mount_path_2, mount_path_3, "enter custom path"], default=mount_path_3)
-
+    if local_mount_point == "enter custom path": local_mount_point = input("enter custom path = ")
     PROGRAM_PATH.write_text(f"""
     remote_server={remote_server}
     share_path={share_path}
