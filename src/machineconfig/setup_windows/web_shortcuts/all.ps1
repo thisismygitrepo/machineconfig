ff01ef0f1495f68d333e4215d41285b86122b35d
Alex Al-Saffar <alex.al-saffar@sa.gov.au>
2023-08-03 22:56:38 +0930

    chore(all.ps1): update variable name from $machineconfig_path to $machineconfig chore(all.ps1): update script execution syntax for symlinks and devapps

--- src/machineconfig/setup_windows/web_shortcuts/all.ps1
+++ src/machineconfig/setup_windows/web_shortcuts/all.ps1
@@ -6,9 +6,9 @@ Invoke-WebRequest https://raw.githubusercontent.com/thisismygitrepo/machineconfi
 # dev repos
 Invoke-WebRequest https://raw.githubusercontent.com/thisismygitrepo/machineconfig/main/src/machineconfig/setup_windows/repos.ps1 | Invoke-Expression
 # symlinks: locally, run: `ftpsx username@hostname[:port] ~/dotfiles -z`, then, on the remote:
-$machineconfig_path = (python -c "print(__import__('machineconfig').__file__[:-12])")
-. $machineconfig/setup_windows/symlinks.ps1
+$machineconfig = (python -c "print(__import__('machineconfig').__file__[:-12])")
+. "$machineconfig/setup_windows/symlinks.ps1"
 # pwsh profile
 # $machineconfig/setup_windows/wt_and_pwsh.ps1  # experimental
 # devapps:
-$machineconfig/setup_windows/devapps.ps1
+. "$machineconfig/setup_windows/devapps.ps1"
