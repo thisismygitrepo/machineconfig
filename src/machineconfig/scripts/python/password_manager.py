10d2d4e6fdf987a2d38e4401275ef327cef2d65f
Alex Al-Saffar <programmer@usa.com>
2023-09-15 16:10:26 +0930

    chore: remove unused Bitwarden CLI installer script
    feat(password_manager.py): add Bitwarden CLI installer function to install the CLI tool globally

--- src/machineconfig/scripts/python/password_manager.py
+++ src/machineconfig/scripts/python/password_manager.py
@@ -6,6 +6,7 @@ import subprocess
 import clipboard
 import argparse
 import time
+from typing import Optional
 
 
 def main():
@@ -25,6 +26,23 @@ def main():
 
     clipboard.copy(pwd)
     print(f"✅ Password {args.name} copied to clipboard 🖇️.")
+
+
+def main_installer(version: Optional[str] = None):
+    """Bitwarden CLI installer
+    "~/AppData/Roaming/Bitwarden CLI/data.json";
+    npm install -g @bitwarden/cli
+    """
+    import crocodile.toolbox as tb
+    import platform
+    _ = version
+    if platform.system() == "Windows":
+        res = tb.P(r"https://vault.bitwarden.com/download/?app=cli&platform=windows").download().unzip(inplace=True)
+        res.search("bw.exe").move(tb.P.get_env().WindowsApps)
+        res.delete(sure=True)
+    elif platform.system() == "Linux":
+        raise NotImplementedError("Linux not implemented yet")
+    return None
 
 
 if __name__ == '__main__':
