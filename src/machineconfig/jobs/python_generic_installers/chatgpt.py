1bee78094c4938da4fec0f4fb9992296f6f82be2
Alex Al-Saffar <programmer@usa.com>
2023-09-23 16:46:31 +0930

    fix(chatgpt.py): use inplace=True when unzipping and untarring downloaded files to improve efficiency feat(cloud_manager.py): add support for additional command line arguments -r, --reset_local and -m, --monitor_cloud to provide more flexibility in managing cloud resources

--- src/machineconfig/jobs/python_generic_installers/chatgpt.py
+++ src/machineconfig/jobs/python_generic_installers/chatgpt.py
@@ -19,10 +19,10 @@ def main(version: Optional[str] = None) -> None:
         print(f"Could not find browsh release for version {version}")
         return None
     if system() == "Windows":
-        downloaded = latest.joinpath("chatgpt_Windows_x86_64.zip").download().unzip()
+        downloaded = latest.joinpath("chatgpt_Windows_x86_64.zip").download().unzip(inplace=True)
         find_move_delete_windows(downloaded=downloaded)
     elif system() == "Linux":
-        downloaded = latest.joinpath("chatgpt_Linux_x86_64.tar.gz").download().ungz_untar()
+        downloaded = latest.joinpath("chatgpt_Linux_x86_64.tar.gz").download().ungz_untar(inplace=True)
         find_move_delete_linux(downloaded=downloaded, tool_name="chatgpt")
 
 
--- src/machineconfig/scripts/python/cloud_manager.py
+++ src/machineconfig/scripts/python/cloud_manager.py
@@ -8,11 +8,16 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--cloud", help="Rclone Config Name", type=str, default=None)
-
+    parser.add_argument("-c", "--cloud", help="Rclone Config Name", action="store", type=str, default=None)
+    parser.add_argument("-r", "--reset_local", help="Clear local cache", action="store_true", default=False)
+    parser.add_argument("-m", "--monitor_cloud", help="Monitor workers instead of running a job server.", action="store_true", default=False)
     args = parser.parse_args()
-    cm = CloudManager(max_jobs=1, cloud=args.cloud)
-    cm.run()
+
+    cm = CloudManager(max_jobs=1, cloud=args.cloud, reset_local=args.reset_local)
+    if args.monitor_cloud:
+        cm.run_monitor()
+    else:
+        cm.run()
 
 
 if __name__ == '__main__':
