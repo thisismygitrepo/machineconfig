4e4f184f47a9b9d3be5fde9c119d605a41ebf4ff
Alex Al-Saffar <programmer@usa.com>
2023-09-28 19:21:47 +0930

    working towards cloud-based workload scheduler

--- src/machineconfig/setup_windows/web_shortcuts/ascii_art.ps1
+++ src/machineconfig/setup_windows/web_shortcuts/ascii_art.ps1
@@ -2,7 +2,7 @@
 
 # below, check if npm is installed @ C:\Program Files\nodejs\npm
 if (Test-Path -Path "C:\Program Files\nodejs\npm") {
-    echo "npm already installed ✅"
+    Write-Output "npm already installed ✅"
 } else {
     winget install --Id CoreyButler.NVMforWindows
 }
@@ -10,7 +10,7 @@ if (Test-Path -Path "C:\Program Files\nodejs\npm") {
 
 # test path ~/AppData/Roaming/npm/lolcatjs then install if not found
 if (Test-Path -Path "~/AppData/Roaming/npm/lolcatjs") {  # may be ensure that utility.cmd is available
-    echo "lolcatjs already installed ✅"
+    Write-Output "lolcatjs already installed ✅"
 } else {
     # install nodejs
     npm install -g lolcatjs  # -g is essential to have cli utility as well as js library.
@@ -21,14 +21,14 @@ if (Test-Path -Path "~/AppData/Roaming/npm/lolcatjs") {  # may be ensure that ut
 if (Test-Path -Path "~/AppData/Roaming/npm/figlet") {  # may be ensure that utility.cmd is available
     # all good
 } else {
-    echo "figlet already installed ✅"
+    Write-Output "figlet already installed ✅"
     npm install -g figlet-cli  # -g is essential to have cli utility as well as js library.
 }
 
 
 # install boxes if not found
 if (Test-Path -Path "~/AppData/Local/Microsoft/WindowsApps/boxes.exe") {  # may be ensure that utility.cmd is available
-    echo "boxes already installed ✅"
+    Write-Output "boxes already installed ✅"
 } else {
     ~/scripts/activate_ve
     python -m fire machineconfig.jobs.python_windows_installers.boxes main
