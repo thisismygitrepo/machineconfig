f297210795a67b0c6043b565bcb4885749fe405e
Alex Al-Saffar <programmer@usa.com>
2023-10-01 11:09:25 +1030

    fixed a repos

--- src/machineconfig/scripts/python/cloud_manager.py
+++ src/machineconfig/scripts/python/cloud_manager.py
@@ -9,6 +9,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cloud", help="Rclone Config Name", action="store", type=str, default=None)
+    parser.add_argument("-s", "--serve", help="Start job server", action="store_true", default=False)
     parser.add_argument("-R", "--reset_local", help="Clear local cache", action="store_true", default=False)
     parser.add_argument("-r", "--rerun_jobs", help="Re-run jobs by bringing them back from wherever to the queue.", action="store_true", default=False)
     parser.add_argument("-f", "--queue_failed_jobs", help="Bring failed jobs back to queued jobs for a re-trial.", action="store_true", default=False)
@@ -17,13 +18,13 @@ def main():
     args = parser.parse_args()
 
     cm = CloudManager(max_jobs=args.num_jobs, cloud=args.cloud, reset_local=args.reset_local)
+    if args.queue_failed_jobs:
+        cm.clean_failed_jobs_mess()
+    if args.rerun_jobs:
+        cm.rerun_jobs()
     if args.monitor_cloud:
         cm.run_monitor()
-    elif args.queue_failed_jobs:
-        cm.clean_failed_jobs_mess()
-    elif args.rerun_jobs:
-        cm.rerun_jobs()
-    else:
+    if args.serve:
         cm.run()
 
 
