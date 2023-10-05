14e55dd445947f22e0ce704a983be94bc207a656
Alex Al-Saffar <programmer@usa.com>
2023-10-05 13:19:40 +1030

    cleaning

--- src/machineconfig/scripts/python/devops_add_ssh_key.py
+++ src/machineconfig/scripts/python/devops_add_ssh_key.py
@@ -14,17 +14,20 @@ def get_add_ssh_key_script(path_to_key: tb.P):
 
     if authorized_keys.exists():
         split = "\n"
-        print(f'Users that can access this machine have private keys of those pub keys:\n{authorized_keys.read_text().split(split)}')
+        print(f'ℹ️ Users that can access this machine have private keys of those pub keys:\n{authorized_keys.read_text().split(split)}')
 
         if path_to_key.read_text() in authorized_keys.read_text():
+            print(f"⚠️ Key {path_to_key} already added to {authorized_keys}. No action is taken.")
             program = ""
         else:
             if system() == "Linux":
                 program = f"cat {path_to_key} >> ~/.ssh/authorized_keys"
-            else:
-                program = LIBRARY_ROOT.joinpath("setup_windows/openssh-server_add-sshkey.ps1")
-                program = tb.P(program).expanduser().read_text().replace('$sshfile=""', f'$sshfile="{path_to_key}"')
-
+            elif system() == "Windows":
+                program_path = LIBRARY_ROOT.joinpath("setup_windows/openssh-server_add-sshkey.ps1")
+                program = program_path.expanduser().read_text()
+                assert "$sshfile=" in program, f"This section performs string manipulation on the script {program_path} to add the key to the authorized_keys file. The script has changed and the string '$sshfile=' is not found."
+                program = program.replace('$sshfile = ""', f'$sshfile = "{path_to_key}"')
+            else: raise NotImplementedError
     else:
         if system() == "Linux":
             program = f"cat {path_to_key} > ~/.ssh/authorized_keys"
@@ -46,12 +49,16 @@ sudo service ssh --full-restart
 
 def main():
     pub_keys = tb.P.home().joinpath(".ssh").search("*.pub")
-    res = display_options("Which public key to add? ", options=pub_keys.apply(str).list + [f"all pub keys available ({len(pub_keys)})", "I have the path to the key file", "I want to paste the key itself"])
+    all_keys_option = f"all pub keys available ({len(pub_keys)})"
+    i_have_path_option = "I have the path to the key file"
+    i_paste_option = "I want to paste the key itself"
+    res = display_options("Which public key to add? ", options=pub_keys.apply(str).list + [all_keys_option, i_have_path_option, i_paste_option])
     assert isinstance(res, str), f"Got {res} of type {type(res)} instead of str."
-    if res == "all": program = "\n\n\n".join(pub_keys.apply(get_add_ssh_key_script))
-    elif res == "I have the path to the key file": program = get_add_ssh_key_script(tb.P(input("Path: ")).expanduser().absolute())
-    elif res == "I want to paste the key itself": program = get_add_ssh_key_script(tb.P.home().joinpath(f".ssh/{input('file name (default: my_pasted_key.pub): ') or 'my_pasted_key.pub'}").write_text(input("Paste the pub key here: ")))
-    else: program = get_add_ssh_key_script(tb.P(res))
+    if res == all_keys_option: program = "\n\n\n".join(pub_keys.apply(get_add_ssh_key_script))
+    elif res == i_have_path_option: program = get_add_ssh_key_script(tb.P(input("Path: ")).expanduser().absolute())
+    elif res == i_paste_option: program = get_add_ssh_key_script(tb.P.home().joinpath(f".ssh/{input('file name (default: my_pasted_key.pub): ') or 'my_pasted_key.pub'}").write_text(input("Paste the pub key here: ")))
+    else:
+        program = get_add_ssh_key_script(tb.P(res))
     return program
 
 
