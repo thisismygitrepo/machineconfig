48b99d88bb9023bcab8904d4c82b4467f0f4dcc2
Alex Al-Saffar <programmer@usa.com>
2023-10-07 17:39:59 +1030

    working on perofrmance results

--- src/machineconfig/scripts/python/start_terminals.py
+++ src/machineconfig/scripts/python/start_terminals.py
@@ -2,18 +2,22 @@
 """Script to start terminals on windows and wsl
 """
 
-from machineconfig.utils.utils import PROGRAM_PATH, display_options, install_n_import, get_ssh_hosts
+from machineconfig.utils.utils import PROGRAM_PATH, display_options, install_n_import, get_ssh_hosts, platform
+from typing import Literal, TypeAlias
 
 
-THIS_MACHINE = "THIS_MACHINE"
+THIS_MACHINE = "this"
+THIS_MACHINE_WSL = "thiswsl"
+THIS_MACHINE_HOSTNAME = platform.node()
+THIS_MACHINE_HOSTNAME_WSL = f"{THIS_MACHINE_HOSTNAME}wsl"
+ORIENTATION: TypeAlias = Literal["vertical", "horizontal"]
 
 
-def main_windows_and_wsl(hosts: list[str], orientation: str = "vertical", mprocs: bool = False):
+def main_windows_and_wsl(window: int, hosts: list[str], orientation: ORIENTATION = "vertical", mprocs: bool = False):
     orientation_oposite = "horizontal" if orientation == "vertical" else "vertical"
     orientation_swap                = "up" if orientation         == "horizontal" else "left"
     orientation_opposite_move_focus = "up" if orientation_oposite == "horizontal" else "left"
     orientation_opposite_move_focus_other = "down" if orientation_oposite == "horizontal" else "right"
-    window = 500
     sleep = 3
     sep = f"\nsleep {sleep}; wt --window {window}"  # or '`;'
     ssh_cmd = f"-t 'mprocs'" if mprocs else ''  # 'wsl_ssh_windows_port_forwarding.ps1'
@@ -25,14 +29,14 @@ def main_windows_and_wsl(hosts: list[str], orientation: str = "vertical", mprocs
             host_wind = hosts[0] if "wsl" not in hosts[0] else hosts[0].split("wsl")[0]
             host_linux = f"{host_wind}wsl"
             cmd = f"""
-wt --window {window} --title {hosts[0]}Windows powershell -Command "ssh {host_linux} {ssh_cmd}" `; split-pane --{orientation} --title {hosts[0]}wsl --size 0.5 powershell -Command "ssh {host_wind} `; split-pane --{orientation_oposite} --size 0.5 powershell "
+wt --window {window} --title {hosts[0]} powershell -Command "ssh {host_linux} {ssh_cmd}" `; split-pane --{orientation} --title {hosts[0]}wsl --size 0.5 powershell -Command "ssh {host_wind} `; split-pane --{orientation_oposite} --size 0.5 powershell "
 """
         else:  # its a windows machine without wsl
-            cmd = f"""wt --window {window} --title {hosts[0]}Windows powershell -Command "ssh {hosts[0]} {ssh_cmd}" `; split-pane --{orientation} --title {hosts[0]}wsl --size 0.1 powershell """
+            cmd = f"""wt --window {window} --title {hosts[0]} powershell -Command "ssh {hosts[0]} {ssh_cmd}" `; split-pane --{orientation} --title {hosts[0]}wsl --size 0.1 powershell """
 
     elif len(hosts) > 1:
         pane_cmd = f'powershell -Command "ssh {hosts[0]} {ssh_cmd}" ' if hosts[0] != THIS_MACHINE else ''
-        cmd = f"""wt --window {window} --title {hosts[0]}_Windows {pane_cmd} """
+        cmd = f"""wt --window {window} --title {hosts[0]} {pane_cmd} """
         for a_host in hosts[1:]:
             if a_host != THIS_MACHINE: pane_cmd = f'powershell -Command "ssh {a_host} {ssh_cmd}" '
             else: pane_cmd = 'powershell'
@@ -51,18 +55,17 @@ wt --window {window} --title {hosts[0]}Windows powershell -Command "ssh {host_li
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    # parser.add_argument("-p", "--path", type=str, help="The directory containing the jobs", default=".")
-    # # optional flag for interactivity
     parser.add_argument("--vertical", "-V", action="store_true", help="Switch orientation to vertical from default horizontal")
-    # parser.add_argument("--debug", "-d", action="store_true", help="debug")
-    # parser.add_argument("--remote", "-r", action="store_true", help="launch on a remote machine")
-    # parser.add_argument("--main", "-m", action="store_true", help="launch the main file")
-    # parser.add_argument("--history", "-h", action="store_true", help="choose from history")
+    parser.add_argument("--window", "-w", type=int, help="The window id to use", default=0)  # 0 refers to this window.
+    parser.add_argument("--hosts", "-H", type=str, nargs="*", help="The hosts to connect to", default=None)
     args = parser.parse_args()
 
-    hosts = display_options(msg="", options=get_ssh_hosts() + [THIS_MACHINE], multi=True, fzf=True)
+    if args.hosts is None: hosts = display_options(msg="", options=get_ssh_hosts() + [THIS_MACHINE], multi=True, fzf=True)
+    else:
+        print("Using provided hosts", args.hosts)
+        hosts = args.hosts
     assert isinstance(hosts, list)
-    cmd = main_windows_and_wsl(hosts=hosts, orientation="vertical" if args.vertical else "horizontal")
+    cmd = main_windows_and_wsl(window=args.window, hosts=hosts, orientation="vertical" if args.vertical else "horizontal")
     print(cmd)
     install_n_import("clipboard").copy(cmd)
     PROGRAM_PATH.write_text(cmd)
