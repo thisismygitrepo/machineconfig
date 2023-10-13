81d3f3644c1e7a8397fba27f74642ea4013d4527
Alex Al-Saffar <programmer@usa.com>
2023-10-13 16:27:03 +1030

    added ipy config

---
+++ src/machineconfig/settings/ipy/magic/myextention.py
@@ -0,0 +1,13 @@
+
+"""G
+"""
+
+from crocodile.core import Display
+from IPython.core.magic import register_line_magic
+import pandas as pd
+ 
+
+@register_line_magic("human")  # type: ignore
+def make_dataframe_human_readable(df: 'pd.DataFrame'):
+    df2 = df.copy()
+    return df2
