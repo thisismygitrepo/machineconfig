c4ceb613ee8ddc2ba15763dfa2fb505bb409da0c
Alex Al-Saffar <programmer@usa.com>
2023-10-15 20:07:18 +1030

    print enchancement

--- src/machineconfig/jobs/python/checkout_version.py
+++ src/machineconfig/jobs/python/checkout_version.py
@@ -14,7 +14,7 @@ def get_ps1_install_template(version: str, repo_root: P, py_version: str):
     template = f"""
 $ve_name = '{repo_root.name}-{version}-prod'
 $py_version = {py_version}  # type: ignore
-Invoke-WebRequest bit.ly/cfgvewindows | Invoke-Expression
+(Invoke-WebRequest bit.ly/cfgvewindows).Content | Invoke-Expression
 activate_ve $ve_name
 cd '{repo_root.collapseuser().joinpath(f"versions/{version}").as_posix()}'
 pip install -r requirements_{platform.system().lower()}.txt
@@ -41,18 +41,18 @@ def checkout_version(version: str, repo_root: P):
     ve_specs = get_ve_specs(ve_path)
     py_version = ve_specs['version_major_minor']
 
-    tmplate_ps1 = get_ps1_install_template(version, repo_root, py_version=py_version)
-    tmplate_bash = get_bash_install_template(version, repo_root, py_version=py_version)
     if platform.system() == "Windows":
+        tmplate_ps1 = get_ps1_install_template(version, repo_root, py_version=py_version)
         repo_root.joinpath("versions", version).create().joinpath("install.ps1").write_text(tmplate_ps1)
     elif platform.system() == "Linux":
+        tmplate_bash = get_bash_install_template(version, repo_root, py_version=py_version)
         repo_root.joinpath("versions", version).create().joinpath("install.sh").write_text(tmplate_bash)
     else: raise NotImplementedError
     target_dir = repo_root.expanduser().absolute().joinpath(f"versions/{version}").as_posix()
     Terminal().run_script(f"""
 cd '{target_dir}'
-. activate_ve {ve_name}
-pip freeze > requirements_{platform.system().lower()}.txt""", shell="pwsh").print()
+. $HOME/scripts/activate_ve {ve_name}
+pip freeze > requirements_{platform.system().lower()}.txt""").print()
     print(f"✅ Installed requirements for version {version}.")
 
 
