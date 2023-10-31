0ad7c642917a33b0ba12e10766abd61df25336f6
Alex Al-Saffar <programmer@usa.com>
2023-10-31 16:32:12 +1030

    wokring on more features

--- src/machineconfig/scripts/windows/wsl_windows_transfer.ps1
+++ src/machineconfig/scripts/windows/wsl_windows_transfer.ps1
@@ -1,6 +1,7 @@
 
 #~/venvs/ve/Scripts/Activate.ps1
-. $PSScriptRoot/activate_ve.ps1
-python $PSScriptRoot/../python/transfer_wsl_win.py $args
+. $PSScriptRoot/activate_ve.ps1 've'
+# python $PSScriptRoot/../python/wsl_windows_transfer.py $args
+python -m machineconfig.scripts.python.wsl_windows_transfer $args
 deactivate -ErrorAction SilentlyContinue
 
