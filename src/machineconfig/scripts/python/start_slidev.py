1d48581bb8c5d0417e9d6cf769d192e8f6c361e8
Alex Al-Saffar <programmer@usa.com>
2024-02-07 16:21:33 +1030

    minor changes

--- src/machineconfig/scripts/python/start_slidev.py
+++ src/machineconfig/scripts/python/start_slidev.py
@@ -5,6 +5,7 @@ slidev
 
 from machineconfig.utils.utils import CONFIG_PATH, PROGRAM_PATH, print_code
 from crocodile.meta import Terminal, P
+# from crocodile.environment import get_network_addresses
 import subprocess
 import platform
 
@@ -12,10 +13,10 @@ import platform
 PORT_DEFAULT = 3030
 
 
-root = CONFIG_PATH.joinpath(".cache/slidev")
-if not root.joinpath("components").exists():
+SLIDEV_REPO = CONFIG_PATH.joinpath(".cache/slidev")
+if not SLIDEV_REPO.joinpath("components").exists():
     # assert slidev is installed first
-    Terminal(stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE).run(f"cd {root.parent};npm init slidev")
+    Terminal(stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE).run(f"cd {SLIDEV_REPO.parent};npm init slidev")
 
 
 def jupyter_to_markdown(file: P):
@@ -69,17 +70,34 @@ def main() -> None:
 
     assert report_dir.exists(), f"{report_dir} does not exist"
     assert report_dir.is_dir(), f"{report_dir} is not a directory"
-    assert report_dir.joinpath("slides.md").exists(), f"slides.md not found in {report_dir}"
-    report_dir.search().apply(lambda x: x.copy(folder=root, overwrite=True))
+
+    md_file = report_dir.joinpath("slides.md")
+    if not md_file.exists():
+        res = report_dir.search("*.md")
+        if len(res) == 1:
+            md_file = res.list[0]
+        else:
+            raise FileNotFoundError(f"slides.md not found in {report_dir}")
+
+    report_dir.search().apply(lambda x: x.copy(folder=SLIDEV_REPO, overwrite=True))
+    if md_file.name != "slides.md":
+        SLIDEV_REPO.joinpath(md_file.name).with_name(name="slides.md", inplace=True, overwrite=True)
 
     # from machineconfig.utils.utils import check_tool_exists
     # check_tool_exists(tool_name="slidev", install_script="npm i -g @slidev/cli")
 
+    import socket
+    try: local_ip_v4 = socket.gethostbyname(socket.gethostname() + ".local")  # without .local, in linux machines, '/etc/hosts' file content, you have an IP address mapping with '127.0.1.1' to your hostname
+    except Exception:
+        print(f"Warning: Could not get local_ip_v4. This is probably because you are running a WSL instance")  # TODO find a way to get the local_ip_v4 in WSL
+        local_ip_v4 = socket.gethostbyname(socket.gethostname())
+
     print(f"Presentation is served at http://{platform.node()}:{port}")
-    program: str = f"cd {root}; slidev --port {port} --remote 0.0.0.0"
+    print(f"Presentation is served at http://localhost:{port}")
+    print(f"Presentation is served at http://{local_ip_v4}:{port}")
+    program: str = f"cd {SLIDEV_REPO}; slidev --port {port} --remote 0.0.0.0; cd {P.cwd()}"
     PROGRAM_PATH.write_text(program)
     print_code(program, lexer="bash")
-
 
 
 if __name__ == '__main__':
