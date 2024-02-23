9cac3bafb5ae3568bd610aadff78ed1917279255
Alex Al-Saffar <programmer@usa.com>
2024-02-23 14:26:09 +1030

    fixed wezterm

--- src/machineconfig/jobs/script_installer/wezterm.py
+++ src/machineconfig/jobs/script_installer/wezterm.py
@@ -2,28 +2,34 @@
 """wezterm installer
 """
 
-from machineconfig.utils.installer import get_installers
+from machineconfig.utils.installer import Installer
 from typing import Optional
 import platform
+
+wezterm = {
+        "repo_url": "CUSTOM",
+        "doc": "cross-platform terminal emulator",
+        "filename_template_windows_amd_64": "WezTerm-windows-{}.zip",
+        "filename_template_linux_amd_64": "wezterm-{}.Ubuntu22.04.deb",
+        "strip_v": False,
+        "exe_name": "wezterm"
+    }
 
 
 def main(version: Optional[str]):
     if platform.system() == "Windows":
         program = "winget install wez.wezterm"
     elif platform.system() == "Linux":
-        insts = get_installers(system=platform.system(), dev=False)
+        inst = Installer.from_dict(d=wezterm, name="wezterm")
         program = ""
-        for inst in insts:
-            if "wezterm" in inst.name:
-                # as per https://wezfurlong.org/wezterm/install/linux.html#installing-on-ubuntu-and-debian-based-systems
-                downloaded, version_to_be_installed = inst.download(version=version)
-                _= version_to_be_installed
+        # as per https://wezfurlong.org/wezterm/install/linux.html#installing-on-ubuntu-and-debian-based-systems
+        downloaded, version_to_be_installed = inst.download(version=version)
+        _= version_to_be_installed
 
-                program = f"""
+        program = f"""
 sudo apt install -y {downloaded}
 rm {downloaded}
 """
-                break
     else:
         raise NotImplementedError("unsupported platform")
     return program
