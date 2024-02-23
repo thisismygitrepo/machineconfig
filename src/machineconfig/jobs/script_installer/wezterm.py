f663a140c0cdd774d1e6610ffb8093b2659c9840
Alex Al-Saffar <programmer@usa.com>
2024-02-23 14:26:59 +1030

    fixed wezterm

--- src/machineconfig/jobs/script_installer/wezterm.py
+++ src/machineconfig/jobs/script_installer/wezterm.py
@@ -7,7 +7,7 @@ from typing import Optional
 import platform
 
 wezterm = {
-        "repo_url": "CUSTOM",
+        "repo_url": "https://github.com/wez/wezterm",
         "doc": "cross-platform terminal emulator",
         "filename_template_windows_amd_64": "WezTerm-windows-{}.zip",
         "filename_template_linux_amd_64": "wezterm-{}.Ubuntu22.04.deb",
