c1caa84eff00fed580e9b5cb0c01be295db532c2
Alex Al-Saffar <programmer@usa.com>
2024-03-12 19:41:43 +1030

    experimenting with wezterm theme

--- src/machineconfig/scripts/python/change_wezterm_theme.py
+++ src/machineconfig/scripts/python/change_wezterm_theme.py
@@ -26,15 +26,13 @@ schemes_list = [
 ]
 
 
-option = choose_one_option(options=schemes_list, header="Choose a theme for Wezterm", fzf=True)
+def main():
+    option = choose_one_option(options=schemes_list, header="Choose a theme for Wezterm", fzf=True)
+    txt_lines = P("~/.config/wezterm/wezterm.lua").expanduser().read_text().splitlines()
+    res_lines = []
+    for line in txt_lines:
+        if 'config.color_scheme = ' in line:
+            res_lines.append(f"config.color_scheme = '{option}'")
+        else: res_lines.append(line)
 
-
-
-txt_lines = P("~/.config/wezterm/wezterm.lua").expanduser().read_text().splitlines()
-res_lines = []
-for line in txt_lines:
-    if 'config.color_scheme = ' in line:
-        res_lines.append(f"config.color_scheme = '{option}'")
-    else: res_lines.append(line)
-
-P("~/.config/wezterm/wezterm.lua").expanduser().write_text('\n'.join(res_lines))
+    P("~/.config/wezterm/wezterm.lua").expanduser().write_text('\n'.join(res_lines))
