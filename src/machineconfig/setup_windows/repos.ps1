14e91cdfb5beff2f31f8eebacf3bf727a2fe642a
Alex Al-Saffar <programmer@usa.com>
2024-05-15 16:19:50 +0930

    house keepign

--- src/machineconfig/setup_windows/repos.ps1
+++ src/machineconfig/setup_windows/repos.ps1
@@ -7,6 +7,8 @@ mkdir code -ErrorAction SilentlyContinue
 cd ~/code
 
 winget install --no-upgrade --name "Git" --Id Git.Git --source winget --accept-package-agreements --accept-source-agreements
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
 
 git clone https://github.com/thisismygitrepo/crocodile.git --depth 4
 git clone https://github.com/thisismygitrepo/machineconfig --depth 4  # Choose browser-based authentication.
