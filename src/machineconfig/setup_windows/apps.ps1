28401658b481f75a66c47ef9616f81c2f1d8b5d8
Alex Al-Saffar <programmer@usa.com>
2024-06-19 12:03:51 +0930

    added linux brew

--- src/machineconfig/setup_windows/apps.ps1
+++ src/machineconfig/setup_windows/apps.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = "Stop"
 
 # ================================= APPS ================================================
-# winget install --no-upgrade --name "Google Chrome" --Id "Google.Chrome" --source winget --accept-package-agreements --accept-source-agreements
+#winget install --no-upgrade --name "Google Chrome" --Id "Google.Chrome" --source winget --accept-package-agreements --accept-source-agreements
 winget install --no-upgrade --name "Brave" --Id Brave.Brave --source winget
 #winget install --no-upgrade --name "Chrome Remote Desktop Host" --Id "Google.ChromeRemoteDesktop" --source winget
 #winget install --no-upgrade --name Zoom --Id Zoom.Zoom --source winget
@@ -14,10 +14,11 @@ winget install --no-upgrade --name "7-zip" --Id "7zip.7zip" --source winget --ac
 #winget install --no-upgrade --name "Adobe Acrobat Reader DC" --Id "dobe.Acrobat.Reader.64-bit" --source winget
 #winget install --no-upgrade --name "Mozilla Firefox" --Id "Mozilla.Firefox" --source winget --accept-package-agreements --accept-source-agreements
 #winget install --no-upgrade --name "Mozilla Thunderbird" --Id "Mozilla.Thunderbird" --source winget --accept-package-agreements --accept-source-agreements
-# winget install --no-upgrade --name "Microsoft Garage Mouse without Borders" --Id "Microsoft.MouseWithoutBorders" --source winget --accept-package-agreements --accept-source-agreements
+#winget install --no-upgrade --name "Microsoft Garage Mouse without Borders" --Id "Microsoft.MouseWithoutBorders" --source winget --accept-package-agreements --accept-source-agreements
 #winget install --no-upgrade --name "StreamlabsOBS" --Id "Streamlabs.StreamlabsOBS" --source winget --accept-package-agreements --accept-source-agreements
 #winget install --no-upgrade --name "OBSStudio" --Id "OBSProject.OBSStudio" --source winget --accept-package-agreements --accept-source-agreements
-#winget install --no-upgrade --name "MiKTeX --Id "MiKTeX.MiKTeX"  --source winget  # library / lanugage
+#winget install --no-upgrade --name "MiKTeX" --Id "MiKTeX.MiKTeX"  --source winget  # library / lanugage
+#winget install --no-upgrade --name "perl" -e --id "StrawberryPerl.StrawberryPerl"  --source winget --accept-package-agreements --accept-source-agreements
 #winget install --no-upgrade --name "TexMaker" --Id "Texmaker.Texmaker" --source winget  # IDE better than simple TexWorks shipped with MikTex. IDE is basically GUI for cmd interface of Tex
 
 #winget install --no-upgrade --name "anaconda3" --source winget
@@ -44,7 +45,7 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 winget install --no-upgrade --name "Powershell" --Id "Microsoft.PowerShell" --source winget  # powershell require admin
 
 # ======================= Terminal-based editors =================================
-# winget install --no-upgrade --name "Lapce" --Id Lapce.Lapce  # window app variant of helix
+#winget install --no-upgrade --name "Lapce" --Id Lapce.Lapce  # window app variant of helix
 winget install --no-upgrade --name "Neovim" --Id Neovim.Neovim --source winget --accept-package-agreements --accept-source-agreements
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 Invoke-WebRequest "https://spacevim.org/install.cmd" -OutFile "~/Downloads/spacevim_installer.cmd"
@@ -63,28 +64,28 @@ winget install --no-upgrade --name "Ubuntu" --Id "Canonical.Ubuntu.2204" --sourc
 winget install --no-upgrade --name "VSRedistrib" --Id "Microsoft.VC++2015-2022Redist-x64" --source winget -e
 winget install --no-upgrade --name "VSBuildTools" --Id "Microsoft.VisualStudio.2022.BuildTools" --source winget -e
 winget install --no-upgrade --name "Rustup" --Id Rustlang.Rustup --source winget
-# winget install --no-upgrade --name "julia" --Id "Julialang.Julia" --source winget --accept-package-agreements --accept-source-agreements
+#winget install --no-upgrade --name "julia" --Id "Julialang.Julia" --source winget --accept-package-agreements --accept-source-agreements
 
 winget install --no-upgrade --name "Codeblocks" --Id Codeblocks.Codeblocks --source winget  # gives gcc compiler  # could also be provided by MSCV Build Tools
 winget install --no-upgrade --name "GnuWin32: Make" --Id GnuWin32.Make --source winget  # make command  # can be provided by mscv build tools.
 winget install --no-upgrade --name "GnuPG" GnuPG.GnuPG
 
-# winget install --no-upgrade --name "bottom" --Id Clement.bottom --source winget --accept-package-agreements --accept-source-agreements
+#winget install --no-upgrade --name "bottom" --Id Clement.bottom --source winget --accept-package-agreements --accept-source-agreements
 # Move-Item 'C:\Program Files\bottom\bin\btm.exe' $HOME\AppData\Local\Microsoft\WindowsApps\btm.exe  # move to userpath
-# winget install --no-upgrade --name "onefetch" --Id o2sh.onefetch --source winget  # repo-version of system fastfetch, see also tokei
-# winget install --no-upgrade --name "Just" --Id Casey.Just --source winget  # commandline runner
-# winget install --no-upgrade --name Cloudflare --Id Cloudflare.cloudflared --source winget
-# winget install --no-upgrade --name "Cloudflare WARP" --Id "Cloudflare.Warp" --accept-package-agreements --accept-source-agreements --source winget
-# winget install --no-upgrade --name "gitui" --Id gitui --source winget
-# winget install --no-upgrade --name "hyperfine" --Id sharkdp.hyperfine --source winget  # benchmarking tool
-# winget install --no-upgrade --name "AWS Command Line Interface"
-# winget install --no-upgrade --name "Windows Terminal" --Id "Microsoft.WindowsTerminal" --Source winget  # Terminal is is installed by default on W11
-# winget install --no-upgrade --name "wezterm" --Id "wez.wezterm" --source winget
-# winget install --no-upgrade --name "nu" --Id "Nushell.Nushell" --source winget  # add to userpath C:\Program Files\nu\bin, done in symlinks
+#winget install --no-upgrade --name "onefetch" --Id o2sh.onefetch --source winget  # repo-version of system fastfetch, see also tokei
+#winget install --no-upgrade --name "Just" --Id Casey.Just --source winget  # commandline runner
+#winget install --no-upgrade --name Cloudflare --Id Cloudflare.cloudflared --source winget
+#winget install --no-upgrade --name "Cloudflare WARP" --Id "Cloudflare.Warp" --accept-package-agreements --accept-source-agreements --source winget
+#winget install --no-upgrade --name "gitui" --Id gitui --source winget
+#winget install --no-upgrade --name "hyperfine" --Id sharkdp.hyperfine --source winget  # benchmarking tool
+#winget install --no-upgrade --name "AWS Command Line Interface"
+#winget install --no-upgrade --name "Windows Terminal" --Id "Microsoft.WindowsTerminal" --Source winget  # Terminal is is installed by default on W11
+#winget install --no-upgrade --name "wezterm" --Id "wez.wezterm" --source winget
+#winget install --no-upgrade --name "nu" --Id "Nushell.Nushell" --source winget  # add to userpath C:\Program Files\nu\bin, done in symlinks
 winget install --no-upgrade --name "Oh My Posh" --Id "JanDeDobbeleer.OhMyPosh" --source winget
-# winget install --no-upgrade --name "Starship" --Id Starship.Starship --source winget
+#winget install --no-upgrade --name "Starship" --Id Starship.Starship --source winget
 winget install --no-upgrade --name "Git" --Id Git.Git --source winget --accept-package-agreements --accept-source-agreements
-# winget install --no-upgrade --name "GitHub CLI" --Id GitHub.cli --source winget --accept-package-agreements --accept-source-agreements
+#winget install --no-upgrade --name "GitHub CLI" --Id GitHub.cli --source winget --accept-package-agreements --accept-source-agreements
 # as per https://docs.github.com/en/copilot/github-copilot-in-the-cli/using-github-copilot-in-the-cli
 # & 'C:\Program Files\GitHub CLI\gh.exe' extension install github/gh-copilot auth login
 # & 'C:\Program Files\GitHub CLI\gh.exe' extension install github/gh-copilot
