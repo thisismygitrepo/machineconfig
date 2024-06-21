a3140836f31d678d0365ed7cfc1b2a74ea2e4bb4
Alex Al-Saffar <programmer@usa.com>
2024-06-21 09:53:51 +0930

    added linux brew

--- src/machineconfig/jobs/script_installer/lvim.py
+++ src/machineconfig/jobs/script_installer/lvim.py
@@ -16,13 +16,14 @@ def main(version: Optional[str]):
     _ = version
     if platform.system() == "Windows":
         program = """
-pwsh -c "`$LV_BRANCH='release-1.3/neovim-0.9'; iwr https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.ps1 -UseBasicParsing | iex"
+
+pwsh -c "`$LV_BRANCH='release-1.4/neovim-0.9'; iwr https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.ps1 -UseBasicParsing | iex"
 
 """
     elif platform.system() == "Linux":
         program = """
 
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
 """
     else:
