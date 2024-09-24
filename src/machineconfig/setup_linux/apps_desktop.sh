2a56cf17d85406b2acc64b8e8b034c60b44dfcf8
Alex Al-Saffar <programmer@usa.com>
2024-09-24 13:50:24 +0930

    Add installation of Rofi, Rofimoji, and Greenclip in setup_linux script

--- src/machineconfig/setup_linux/apps_desktop.sh
+++ src/machineconfig/setup_linux/apps_desktop.sh
@@ -19,11 +19,24 @@ flatpak install flathub com.github.hluk.copyq --noninteractive
 sudo nala install remmina remmina-plugin-rdp -y
 
 sudo nala install rofie
+sudo nala install pipx
+sudo pipx install rofimoji
+# https://github.com/fdw/rofimoji?tab=readme-ov-file#dependencies
+
+
+wget -P ~/Downloads https://github.com/erebe/greenclip/releases/download/v4.2/greenclip
+chmod +x ~/Downloads/greenclip
+sudo mv ~/Downloads/greenclip /usr/bin/
+# greenclip daemon &
+
+# rofi -modi "emoji:rofimoji" -show emoji
+# rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
+
+
 # rofie -show drun -modi drun -theme ~/.config/rofi/launcher.rasi
 nix-env -iA nixpkgs.rofi-emoji
 # https://github.com/hluk/CopyQ
 # https://github.com/SUPERCILEX/gnome-clipboard-history?tab=readme-ov-file
 # https://github.com/SUPERCILEX/clipboard-history
-
 
 ln -s /home/$USER/.nix-profile/share/applications/* /home/$USER/.local/share/applications/
