107d0cba922be4ac72b7805985f162f5d9ac42a7
Alex Al-Saffar <programmer@usa.com>
2024-10-08 08:34:43 +1030

    Add WezTerm installer for Linux with installation script

---
+++ src/machineconfig/jobs/python_custom_installers/dev/wezterm.py
@@ -0,0 +1,34 @@
+"""wezterm installer
+"""
+
+import platform
+from typing import Optional
+
+
+config_dict = {
+        "repo_url": "CUSTOM",
+        "doc": "cli for wezterm",
+        "filename_template_windows_amd_64": "",
+        "filename_template_linux_amd_64": "",
+        "strip_v": False,
+        "exe_name": "wezterm"
+}
+
+
+def main(version: Optional[str]):
+    _ = version
+    if platform.system() == "Windows":
+        raise NotImplementedError("unsupported platform")
+    elif platform.system() == "Linux":
+        import machineconfig.jobs.python_custom_installers as module
+        from pathlib import Path
+        program = Path(module.__file__).parent.joinpath("scripts/linux/wezterm.sh").read_text(encoding="utf-8")
+    else:
+        raise NotImplementedError("unsupported platform")
+    # _res = Terminal(stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).run_script(script=program, shell="default").print(desc="Running custom installer", capture=True)
+    # run script here as it requires user input
+    return program
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/machineconfig/jobs/python_custom_installers/scripts/linux/wezterm.sh
@@ -0,0 +1,8 @@
+
+# as per https://wezfurlong.org/wezterm/install/linux.html#__tabbed_1_3
+
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+sudo nala update
+sudo nala install wezterm
