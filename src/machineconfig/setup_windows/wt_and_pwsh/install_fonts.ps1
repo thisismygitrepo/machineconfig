729fec9e119eaf2c825fb8240abab9afca6f27e0
Alex Al-Saffar <programmer@usa.com>
2025-02-08 12:14:37 +1030

    Refactor font installation logic to improve detection of already installed fonts and streamline installation process

--- src/machineconfig/setup_windows/wt_and_pwsh/install_fonts.ps1
+++ src/machineconfig/setup_windows/wt_and_pwsh/install_fonts.ps1
@@ -1,32 +1,27 @@
 # to run: powershell.exe -executionpolicy Bypass -nologo -noninteractive -file .\Install_Fonts.ps1
 
-function Test-FontInstalled {
-    param(
-        [string]$FontName
-    )
-    $fonts = Get-ChildItem "C:\Windows\Fonts" | Where-Object {$_.PSIsContainer -eq $false} | Select-Object -ExpandProperty BaseName
-    return $fonts -contains $FontName
-}
-
-if (Test-FontInstalled -FontName "CaskadyiaCove NF") {
-    Write-Host "Font CaskadyiaCove NF is installed. Skipping installation."
-    exit
-}
-
 $FONTS = 0x14
 $Path = ".\fonts-to-be-installed"
 $objShell = New-Object -ComObject Shell.Application
 $objFolder = $objShell.Namespace($FONTS)
 $Fontdir = dir $Path
 
-foreach($File in $Fontdir) {
-    if(!($file.name -match "pfb$")) {
-        $fontName = $File.BaseName
-        if(!(Test-FontInstalled -FontName $fontName)) {
+foreach ($File in $Fontdir) {
+    if (!($file.name -match "pfb$")) {
+        $try = $true
+        $installedFonts = @(Get-ChildItem c:\windows\fonts | Where-Object {$_.PSIsContainer -eq $false} | Select-Object basename)
+        $name = $File.baseName
+
+        foreach ($font in $installedFonts) {
+            $font = $font -replace "_", ""
+            $name = $name -replace "_", ""
+            if ($font -match $name) {
+                $try = $false
+            }
+        }
+
+        if ($try) {
             $objFolder.CopyHere($File.fullname)
-            Write-Host "Installing font: $fontName"
-        } else {
-            Write-Host "Font already installed: $fontName"
         }
     }
 }
