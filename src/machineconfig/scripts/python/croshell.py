412be3bfd3d7043eeeaf1083aa0bfeaafc20eb8c
Alex Al-Saffar <programmer@usa.com>
2025-08-31 09:04:27 +0930

    Implement code changes to enhance functionality and improve performance

--- src/machineconfig/scripts/python/croshell.py
+++ src/machineconfig/scripts/python/croshell.py
@@ -18,7 +18,8 @@ console = Console()
 
 def add_print_header_pycode(path: str, title: str):
     return f"""
-from machineconfig.utils.path_reduced import P as PathExtended 
+# from machineconfig.utils.path_reduced import P as PathExtended 
+from crocodile.file_management import P as PathExtended
 pycode = PathExtended(r'{path}').read_text(encoding="utf-8")
 pycode = pycode.split("except Exception: print(pycode)")[2]
 
