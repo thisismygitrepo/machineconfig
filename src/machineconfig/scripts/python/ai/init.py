9eae090741f28c42f05d4a16d187664a4d67eb61
Alex Al-Saffar <programmer@usa.com>
2025-09-01 08:26:26 +0930

    minor change

---
+++ src/machineconfig/scripts/python/ai/init.py
@@ -0,0 +1,43 @@
+
+
+from pathlib import Path
+from git import Repo
+
+# def add_config_for_curor(repo_root: Path, config_f):
+
+def add_ai_configs(repo_root: Path):
+    import machineconfig as mc
+    mc_root = Path(mc.__file__).parent
+
+    try:
+        _repo_obj = Repo(repo_root)
+    except Exception as e:
+        print(f"Error initializing git repo: {e}")
+        return
+
+    rules_dir = mc_root.joinpath("scripts/python/ai/rules")
+
+    python_rules_file = rules_dir.joinpath("python/dev.md")
+
+    tmp = repo_root.joinpath(".cursor/rules/python_dev.mdc")
+    tmp.parent.mkdir(parents=True, exist_ok=True)
+    tmp.write_text(data=python_rules_file.read_text(), encoding="utf-8")
+
+    # as per: https://docs.github.com/en/copilot/how-tos/configure-custom-instructions/add-repository-instructions
+    # Copilot Chat on github website chat
+    tmp = repo_root.joinpath(".github/copilot-instructions.md")
+    tmp.parent.mkdir(parents=True, exist_ok=True)
+    tmp.write_text(data=python_rules_file.read_text(), encoding="utf-8")
+
+    tmp = repo_root.joinpath(".github/instructions/python01.instructions.md")
+    tmp.parent.mkdir(parents=True, exist_ok=True)
+    tmp.write_text(data=python_rules_file.read_text(), encoding="utf-8")
+
+    tmp = repo_root.joinpath("CLAUDE.md")
+    tmp.write_text(data=python_rules_file.read_text(), encoding="utf-8")
+    tmp = repo_root.joinpath("GEMINI.md")
+    tmp.write_text(data=python_rules_file.read_text(), encoding="utf-8")
+
+
+if __name__ == "__main__":
+    add_ai_configs(repo_root=Path("."))
