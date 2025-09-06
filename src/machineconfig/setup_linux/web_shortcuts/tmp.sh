db512801f252303df8399c085115a683d6f18f78
Alex Al-Saffar <programmer@usa.com>
2025-09-07 00:09:12 +0930

    feat: optimize tmp.sh by copying crouton to /tmp/ before execution

--- src/machineconfig/setup_linux/web_shortcuts/tmp.sh
+++ src/machineconfig/setup_linux/web_shortcuts/tmp.sh
@@ -1,5 +1,2 @@
-
-curl -O https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton
-sudo sh crouton -r bookworm -t core -n debian
-
-sudo enter-chroot -n debian
+cp crouton /tmp/
+sudo sh /tmp/crouton -r bookworm -t core -n debian
