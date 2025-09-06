09c6d3e2498bc5992235bc2038064844eef3fce2
Alex Al-Saffar <programmer@usa.com>
2025-09-06 22:54:09 +0930

    feat: replace wget with curl for downloading debootstrap package

--- src/machineconfig/setup_linux/web_shortcuts/tmp.sh
+++ src/machineconfig/setup_linux/web_shortcuts/tmp.sh
@@ -1,10 +1,5 @@
-
-echo "this is tmp"
-
-wget http://ftp.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.141_all.deb
-
+curl -O http://ftp.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.141_all.deb
 ar x debootstrap_1.0.141_all.deb
 tar -xf data.tar.xz
 sudo cp -a usr/share/debootstrap /usr/share/
 sudo cp usr/sbin/debootstrap /usr/sbin/
-
