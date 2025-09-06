f093ea75c3a9ea34ed0861815ec4d8f2a2f1baa5
Alex Al-Saffar <programmer@usa.com>
2025-09-06 22:51:03 +0930

    feat: restore tmp.sh script with initial content

---
+++ src/machineconfig/setup_linux/web_shortcuts/tmp.sh
@@ -0,0 +1,10 @@
+
+echo "this is tmp"
+
+wget http://ftp.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.141_all.deb
+
+ar x debootstrap_1.0.141_all.deb
+tar -xf data.tar.xz
+sudo cp -a usr/share/debootstrap /usr/share/
+sudo cp usr/sbin/debootstrap /usr/sbin/
+
