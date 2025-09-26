3b32f86237b65e2137196c38f4dd02becd548627
Alex Al-Saffar <programmer@usa.com>
2025-09-26 10:44:09 +0930

    feat: update project version to 3.81 and replace typer with typer-slim in dependencies

--- src/machineconfig/scripts/python/devops_devapps_install.py
+++ src/machineconfig/scripts/python/devops_devapps_install.py
@@ -1,6 +1,7 @@
 """Devops Devapps Install"""
 
 # import subprocess
+import typer
 from rich.progress import Progress, SpinnerColumn, TextColumn
 from machineconfig.utils.source_of_truth import LIBRARY_ROOT
 from machineconfig.utils.options import choose_from_options
@@ -8,15 +9,17 @@ from machineconfig.utils.installer import get_installers, install_all
 from platform import system
 from typing import Any, Optional, Literal, TypeAlias, get_args, Annotated
 
-WHICH_CAT: TypeAlias = Literal["AllEssentials", "EssentialsAndOthers", "SystemInstallers", "PrecheckedCloudInstaller", "CHOOSE"]
+WHICH_CAT: TypeAlias = Literal["AllEssentials", "EssentialsAndOthers", "SystemInstallers", "PrecheckedCloudInstaller"]
 
 
 def main_with_parser():
     import typer
-    typer.run(main)
+    app = typer.Typer()
+    app.command()(main)
+    app()
 
 
-def main(which: Annotated[Optional[str], f"Choose a category or program to install, {list(get_args(WHICH_CAT))} or <program_name>"]) -> None:
+def main(which: Annotated[Optional[str], typer.Argument(help=f"Choose a category or program to install, {list(get_args(WHICH_CAT))} or <program_name>")]) -> None:
     if which is not None and which in get_args(WHICH_CAT):  # install by category
         return get_programs_by_category(program_name=which)  # type: ignore
 
@@ -127,7 +130,7 @@ def get_programs_by_category(program_name: WHICH_CAT):
                 if sub_program.startswith("#winget"):
                     sub_program = sub_program[1:]
                 program += "\n" + sub_program
-        case "CHOOSE": raise NotImplementedError("CHOOSE is not implemented yet.")
+        # case "CHOOSE": raise NotImplementedError("CHOOSE is not implemented yet.")
         # case "OtherDevApps":
         #     installers = get_installers(dev=True, system=system())
         #     options__: list[str] = [x.get_description() for x in tqdm(installers, desc="Checking installed programs")]
