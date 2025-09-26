e8801e7ca6dfb32d76872175a202b314b305a74e
Alex Al-Saffar <programmer@usa.com>
2025-09-26 15:44:13 +0930

    auto_commit_wStHvUDjka

--- src/machineconfig/scripts/python/t4.py
+++ src/machineconfig/scripts/python/t4.py
@@ -1,11 +1,11 @@
 
 import typer
-
+from typing import Annotated
 
 def func(name: str):
     print(f"Hello, {name}! from func")
 
-def hello(name: str):
+def hello(*, name: Annotated[str, typer.Option(..., help="Name to greet")]):
     print(f"Hello, {name}!")
 
 def main():
@@ -13,4 +13,5 @@ def main():
 
 if __name__ == "__main__":
     # typer.run(hello)
+    main()
     pass
