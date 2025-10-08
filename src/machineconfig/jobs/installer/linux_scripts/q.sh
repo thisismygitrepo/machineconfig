bbf6d8e94f8fb85221339de83eed61f5b38f5062
Alex Al-Saffar <programmer@usa.com>
2025-10-08 23:54:22 +1030

    fix: update project version to 5.57 and ensure Downloads directory is created in q.sh

--- src/machineconfig/jobs/installer/linux_scripts/q.sh
+++ src/machineconfig/jobs/installer/linux_scripts/q.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p $HOME/Downloads || true
 cd $HOME/Downloads
 curl --proto '=https' --tlsv1.2 -sSf "https://desktop-release.q.us-east-1.amazonaws.com/latest/q-x86_64-linux.zip" -o "q.zip"
 unzip q.zip
