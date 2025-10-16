d315acaf86d0b184ed7a2edc6766c8c4d8c9cdbf
Alex Al-Saffar <programmer@usa.com>
2025-10-16 10:55:09 +1030

    ch

--- src/machineconfig/setup_windows/web_shortcuts/interactive.ps1
+++ src/machineconfig/setup_windows/web_shortcuts/interactive.ps1
@@ -2,7 +2,7 @@
 
 iex (iwr "https://raw.githubusercontent.com/thisismygitrepo/machineconfig/main/src/machineconfig/setup_windows/uv.ps1").Content
 function mcfg {
-    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.87" mcfg $args
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=6.25" mcfg $args
 }
 function d { mcfg devops @args }
 function c { mcfg cloud @args }
