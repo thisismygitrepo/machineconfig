1c2c7baf7d184a6a06fa3cc2141b7f1413c5ce21
Alex Al-Saffar <programmer@usa.com>
2025-10-16 18:05:12 +1030

    cgh

--- src/machineconfig/cluster/sessions_managers/utils/maker.py
+++ src/machineconfig/cluster/sessions_managers/utils/maker.py
@@ -7,7 +7,6 @@ from pathlib import Path
 def get_fire_command_and_artifact_files(func: FunctionType):
     from machineconfig.utils.meta import function_to_script
     py_script =  function_to_script(func)
-    from pathlib import Path
     from machineconfig.utils.accessories import randstr
     py_script_path = Path.home().joinpath("tmp_results", "tmp_py_scripts", f"tmp_{randstr(10)}.py")
     py_script_path.parent.mkdir(parents=True, exist_ok=True)
@@ -21,8 +20,9 @@ def get_fire_command_and_artifact_files(func: FunctionType):
     return tab_config, py_script_path
 def get_fire_command_and_artifact_files_v2(func: FunctionType):
     import inspect
-    path = inspect.getfile(func)
-    command_to_run = f"""$HOME/.local/bin/fire {path} {func.__name__} """
+    path = Path(inspect.getfile(func))
+    path_relative = path.relative_to(Path.home())
+    command_to_run = f"""$HOME/.local/bin/fire {path_relative} {func.__name__} """
     tab_config: TabConfig = {
         "command": command_to_run,
         "startDir": "$HOME",
