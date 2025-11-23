28f279972ee26905a6771cb26b649df800e20779
Alex Al-Saffar <programmer@usa.com>
2025-11-24 09:46:52 +1030

    out_text = tv_out_path.read_text(encoding="ut

--- src/machineconfig/utils/options.py
+++ src/machineconfig/utils/options.py
@@ -38,8 +38,9 @@ def choose_from_options[T](options: Iterable[T], msg: str, multi: bool, custom_i
         if preview is None:
             preview_line = ""
         elif preview == "bat":
-            preview_line = """--preview-command "bat -n --color=always '{}'" --preview-size 30 """
+            preview_line = r"""--preview-command "bat -n --color=always '{}'" --preview-size 30 """
         tv_cmd = f"""cat {options_txt_path} | tv  {preview_line} --ansi true --source-output "{{strip_ansi}}" > {tv_out_path} """
+        print(f"Running tv command: {tv_cmd}")
         # res = subprocess.run(tv_cmd, shell=True)
         from machineconfig.utils.code import run_shell_script
         res = run_shell_script(tv_cmd, display_script=False, clean_env=False)
