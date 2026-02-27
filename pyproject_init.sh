4fdced357f1d6f99bd358c91ffdfa60f2ff84374
Alex Al-Saffar <programmer@usa.com>
2026-02-27 07:05:59 +0000

    fix: correct variable naming for pattern selection in asset checks

--- pyproject_init.sh
+++ pyproject_init.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+
 uv cache clean --force
 rm -rfd .venv
-uv add --no-cache cryptography fire joblib paramiko randomname requests rich tenacity psutil gitpython rclone-python questionary typer-slim typer
-# uv add --no-cache --group windows pywin32
-uv add --no-cache --group plot sqlalchemy ipykernel ipython jupyterlab kaleido matplotlib nbformat numpy plotly polars python-magic
-uv add --no-cache --dev aider build cleanpy cowsay gdown ipdb ipykernel ipython marimo matplotlib mkdocs mkdocs-autorefs mkdocs-material mkdocstrings mypy numpy pandas plotly polars pre-commit pudb pylint pylsp-mypy pymupdf pypdf pyrefly pyright pytest python-lsp-server qrcode ruff ruff-lsp sqlalchemy textual trogon ty types-mysqlclient types-paramiko types-pytz types-pyyaml types-requests types-sqlalchemy types-toml types-urllib3 vt-py yapf
+uv add --no-cache cryptography fire gitpython joblib paramiko psutil pyyaml questionary randomname rclone-python requests rich tenacity typer
+uv add --no-cache --optional windows pywin32
+uv add --no-cache --optional plot sqlalchemy ipykernel ipython jupyterlab kaleido matplotlib nbformat numpy plotly polars python-magic
+uv add --no-cache --group dev aider build cleanpy cowsay duckdb gdown github-copilot-sdk ipdb ipykernel ipython kaleido marimo matplotlib mkdocs mkdocs-autorefs mkdocs-material mkdocstrings mkdocstrings-python mypy numpy pandas plotly polars pre-commit pudb pydantic pydeps pyinstaller pylint pylsp-mypy pymupdf pypdf pyrefly pyright pytest python-lsp-server qrcode ruff ruff-lsp sqlalchemy textual trogon ty types-mysqlclient types-paramiko types-pytz types-pyyaml types-requests types-sqlalchemy types-toml types-urllib3 vt-py yapf
 uv add --no-cache --group other duckdb-engine pycrucible vt-py
+uv add --no-cache --group plot duckdb ipykernel ipython jupyterlab kaleido matplotlib nbformat numpy plotly polars python-magic sqlalchemy
